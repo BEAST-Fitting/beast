9ec58c1061df9b4c51fdf665eabcf91c4aead2e2
Karl Gordon <karllark@gmail.com>
2013-01-20 15:25:37 -0500

    Moving details of getExtinguishedSED to creategrid to keep with Morgan's vision for code simplicity

--- creategrid.py
+++ creategrid.py
@@ -28,6 +28,61 @@ def make_extinguished_grid(stellar_filename, filter_names, avs, rvs, fbumps):
 
     return g0.getExtinguishedSEDs(filter_names, toPhotLamb=1.0, toFbumpLawLamb=1e-4, Av_vals=avs, Rv_vals=rvs, f_bump_vals=fbumps)
 
+
+    def getExtinguishedSEDs(self, filter_names, toPhotLamb=1.0e4,toFbumpLawLamb=1.0,absFlux=True, Av_vals=numpy.array([3.0]), Rv_vals = numpy.array([2.7]), f_bump_vals = numpy.array([0.0])):
+        """
+        Extinguish and extract fluxes through filters
+        INPUTS:
+              filter_names   list    list of filter names according to the filter lib
+        KEYWORDS:
+              toPhotLamb     float   conversion factor from grid wavelength units to angstroms
+              toFbumpLawLamb float   conversion factor from grid wavelength units to microns
+              Av_vals   numpy array  Av values to iterate over
+              Rv_vals   numpy array  Rv values to iterate over
+              f_bump_vals numpy array f_bump values to iterate over
+        """
+        extLaw = extinction.RvFbumpLaw()
+        
+        if type(filter_names[0]) == str:
+            flist = phot.load_filters(filter_names, interp=True, lamb=self.lamb*toPhotLamb)
+        else:
+            flist = filter_names
+
+        lamb  = numpy.empty( len(flist), dtype=float)
+        for e,k in enumerate(flist):
+            lamb[e] = k.cl
+        
+        Av_vals, Rv_vals, f_bump_vals = numpy.ix_(Av_vals, Rv_vals, f_bump_vals)
+        it = numpy.nditer([Av_vals,Rv_vals,f_bump_vals])
+        ext_lamb = self.lamb*toFbumpLawLamb
+
+        niter = Av_vals.size*Rv_vals.size*f_bump_vals.size
+        cols = {'Av':numpy.empty(self.grid.nrows*niter),'Rv':numpy.empty(self.grid.nrows*niter),'f_bump':numpy.empty(self.grid.nrows*niter)}
+            
+        for key in self.keys():
+            cols[key] = numpy.empty(self.grid.nrows*niter)
+
+        results = MemoryGrid(lamb,seds= numpy.empty((self.grid.nrows*niter,filter_names.__len__())),grid=mytables.Table(iterable=cols,header=self.grid.header))
+
+        itime = time()
+        count = 0
+        print "Memory allocated, starting on " + str(niter) + " iterations"
+        for Av,Rv,f_bump in it:
+            if ((count+1) % 100) == 0:
+                print 100.*count/(Av_vals.size*Rv_vals.size*f_bump_vals.size),time()-itime
+                itime = time()
+            ext = numpy.exp(-1*extLaw.function(ext_lamb,Av=Av,Rv=Rv,f_bump=f_bump))
+            results.grid['Av'][self.grid.nrows*count:self.grid.nrows*(count+1)] = Av
+            results.grid['Rv'][self.grid.nrows*count:self.grid.nrows*(count+1)] = Rv
+            results.grid['f_bump'][self.grid.nrows*count:self.grid.nrows*(count+1)] = f_bump
+            results.seds[self.grid.nrows*count:self.grid.nrows*(count+1)] = phot.extractExtinguishedSEDs(self,flist,ext,absFlux=absFlux)
+            for key in self.keys():
+                results.grid[key][self.grid.nrows*count:self.grid.nrows*(count+1)] = self.grid[key]
+            count += 1
+
+        return results
+
+
 if __name__ == '__main__':
 
     # define the filename with the stellar grid
