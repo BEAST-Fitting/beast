8b9977a87d46e1fbb696ef8fdb6d15246e597a9a
Morgan Fouesneau <fouesneau@mpia.de>
2013-03-04 22:20:34 -0800

    Debug + pep8

--- contextManagers.py
+++ contextManagers.py
@@ -1,5 +1,7 @@
 import time
 import math
+
+
 class timeit(object):
 	""" Time a block of your code.
 	Usage:
@@ -9,17 +11,20 @@ class timeit(object):
 	"""
 	def __init__(self, text=None):
 		self.text = text or ''
+
 	def __enter__(self):
 		print "Timing %s" % (self.text)
 		self.start = time.time()
+
 	def __exit__(self, exc_type, exc_val, exc_tb):
 		self.__pretty_print(time.time() - self.start)
+
 	def __pretty_print(self, t):
-		units = [u"s", u"ms",u'us',"ns"]
+		units = [u"s", u"ms", u'us', "ns"]
 		scaling = [1, 1e3, 1e6, 1e9]
 		if t > 0.0 and t < 1000.0:
 			order = min(-int(math.floor(math.log10(t)) // 3), 3)
-		elif best >= 1000.0:
+		elif t >= 1000.0:
 			order = 0
 		else:
 			order = 3
