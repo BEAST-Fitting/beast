d08ee6cb658847525283f44fa129fa45d507133f
Morgan Fouesneau <morgan@astro.washington.edu>
2013-10-16 17:22:34 -0700

    HDFStore version 1.0
    Fully working version of the class which includes simple in and out interfaces
    
    
    Former-commit-id: ab4ded67e1ca0b63fbd0a40a1490a42020ec2278

--- projects/morgan/hdfstore.py
+++ projects/morgan/hdfstore.py
@@ -1,13 +1,245 @@
 """ HDFStore
 High level interface to PyTables for reading and writing structures to disk
+Could work either as a (key, value) storage or as a classic tables.file.File object.
 
-TODO: easy writing is not their yet
-TODO: get node from a __getitem__ call
-TODO: set node from a __setitem__ call
+    Example usage
+    >>> import numpy as np
+        #make a store
+        with HDFStore('tmp.hd5', mode='w') as hd:
+            #make some variables
+            d = {}
+            d['a'] = np.arange(10, dtype=float)
+            d['b'] = np.arange(10, dtype='int')
+            c = np.random.normal(0, 1, (10, 10))
+            d['c'] = c
+            #put values into the store
+            hd['/subdir/table'] = d
+            hd['/subdir1/array'] = c
+            #check values
+            print hd.keys()
+            print hd['subdir/table']
+            print hd['subdir1/array']
+            hd.removeNode('/subdir1', recursive=True)
+
 """
-
 import tables
 import numpy as np
+
+__all__ = ['HDFStore', 'convert_dict_to_structured_ndarray']
+
+# Type conversion mapping between numpy and pytables
+_typeDict = {}
+_typeDict[np.bool_]      = tables.BoolCol
+_typeDict[np.short]      = tables.Int8Col
+_typeDict[np.int8]       = tables.Int8Col
+_typeDict[np.uint8]      = tables.UInt8Col
+_typeDict[np.int16]      = tables.Int16Col
+_typeDict[np.uint16]     = tables.UInt16Col
+_typeDict[np.int32]      = tables.Int32Col
+_typeDict[np.uint32]     = tables.UInt32Col
+_typeDict[np.int64]      = tables.Int64Col
+_typeDict[np.uint64]     = tables.UInt64Col
+_typeDict[np.float16]    = tables.Float32Col
+_typeDict[np.float32]    = tables.Float32Col
+_typeDict[np.float64]    = tables.Float64Col
+_typeDict[np.complex64]  = tables.Complex64Col
+_typeDict[np.complex128] = tables.Complex128Col
+_typeDict[np.str]        = tables.StringCol
+_typeDict[np.string_]    = tables.StringCol
+_typeDict[str]           = tables.StringCol
+_typeDict[np.unicode_]   = tables.StringCol
+
+
+def __descr_from_dtype__(dtype_, shape_={}):
+    """
+    Get a description instance and byteorder from a (nested) NumPy dtype.
+
+    keywords
+    --------
+    dtype_:  dtype
+        dtype of a ndarray
+
+    shape_:  dict
+        a dictionary of column's shapes
+
+    returns
+    -------
+    r: tables.Description
+        Description of a new table
+    """
+    fields = {}
+    fbyteorder = '|'
+    for (name, (dtype, pos)) in dtype_.fields.items():
+        kind = dtype.base.kind
+        shape = shape_.get(name, ())
+        byteorder = dtype.base.byteorder
+        if byteorder in '><=':
+            if fbyteorder not in ['|', byteorder]:
+                raise NotImplementedError(
+                    "record arrays with mixed byteorders "
+                    "are not supported yet, sorry" )
+            fbyteorder = byteorder
+        # Non-nested column
+        if kind in 'biufSc':
+            #col = tables.Col.from_dtype(dtype, pos=pos)
+            if len(shape) > 1:
+                col = _typeDict[dtype.base.type](shape=shape)
+            else:
+                col = _typeDict[dtype.type]()
+        # Nested column
+        elif kind == 'V' and dtype.shape in [(), (1,)]:
+            col, _ = __descr_from_dtype__(dtype)
+            col._v_pos = pos
+        else:
+            raise NotImplementedError(
+                "record arrays with columns with type description ``%s`` "
+                "are not supported yet, sorry" % dtype )
+        fields[name] = col
+
+    return tables.Description(fields)
+
+
+def __createTable__(hd5, tab, group, tablename):
+    """__createTable__ -- make a new table in the hdf file
+
+    keywords
+    --------
+
+    hd5: tables.file.File
+        opened hdf file
+
+    tab: structured ndarray
+        structure that contains individual columns
+
+    group: str
+        path to where the table will be created
+
+    tablename: str
+        name of the node for the table
+
+    returns
+    -------
+    t: tables.table.Table
+        table node instance already set into the file
+
+    """
+    shapes = {}
+    for k in tab.dtype.names:
+        shapes[k] = tab[k].shape
+    desc = __descr_from_dtype__(tab.dtype, shapes)
+    if group[-1] == '/':
+        group = group[:-1]
+
+    t = hd5.createTable(group, tablename, desc, expectedrows=len(tab), createparents=True)
+    hd5.flush()
+    return t
+
+
+def convert_dict_to_structured_ndarray(data):
+    """convert_dict_to_structured_ndarray
+
+    keywords
+    --------
+
+    data: dictionary like object
+        data structure which provides iteritems and itervalues
+
+    returns
+    -------
+    tab: structured ndarray
+        structured numpy array
+    """
+    newdtype = []
+    for key, dk in data.iteritems():
+        _dk = np.asarray(dk)
+        dtype = _dk.dtype
+        # unknown type is converted to text
+        if dtype.type == np.object_:
+            if len(data) == 0:
+                longest = 0
+            else:
+                longest = len(max(_dk, key=len))
+                _dk = _dk.astype('|%iS' % longest)
+        if _dk.ndim > 1:
+            newdtype.append((str(key), _dk.dtype, (_dk.shape[1],)))
+        else:
+            newdtype.append((str(key), _dk.dtype))
+    tab = np.rec.fromarrays(data.itervalues(), dtype=newdtype)
+    return tab
+
+
+def __write__(data, hd5, group='/', tablename=None, append=False, silent=False, header={}, **kwargs):
+    """__write__
+
+    keywords
+    --------
+    data: dict like object
+        structure that contains individual columns
+
+    hd5: tables.file.File
+        opened hdf file
+
+    group: str
+        path to where the table will be created
+
+    tablename: str
+        name of the node for the table
+
+    append: bool
+        if set, attempts to add content at the end of an existing node
+        Only works for Tables (not Arrays)
+
+    silent: bool
+        if set, do not print information content
+
+    header: dictionary like
+        attributes to add to the node
+    """
+    if tablename in ['', None, 'Noname', 'None']:
+        tablename = 'data'
+
+    #fill the table
+    if not hasattr(data, 'dtype'):
+        #assumes dict like object
+        tab = convert_dict_to_structured_ndarray(data)
+    else:
+        tab = data
+
+    if tab.dtype.names is None:
+        if append:
+            print "Warning: Append has not effect when creating arrays"
+        hd5.createArray(group, tablename, tab, createparents=True)
+        hd5.flush()
+        t = hd5.getNode(group + '/' + tablename)
+    else:
+        #generate the empty table
+        if not append:
+            t = __createTable__(hd5, tab, group, tablename)
+        else:
+            try:
+                t = hd5.getNode(group + '/' + tablename)
+            except tables.NoSuchNodeError:
+                if not silent:
+                    print "Warning: Table does not exists.  New table will be created"
+                t = __createTable__(hd5, tab, group, tablename)
+                try:
+                    t = __createTable__(hd5, tab, group, tablename)
+                except:
+                    if not silent:
+                        print "Warning: Table creation exception. Table may already exist."
+                    t = hd5.getNode(group + '/' + tablename)
+
+        t.append( tab.astype(t.description._v_dtype) )
+        hd5.flush()
+
+    #update the header
+    for k, v in header.iteritems():
+        if k == 'FILTERS' and float(t.attrs['VERSION']) >= 2.0:
+            t.attrs['filters'] = v
+        else:
+            t.attrs[k] = v
+    if not 'TITLE' in header:
+        t.attrs['TITLE'] = tablename
 
 
 class HDFStore(object):
@@ -69,7 +301,7 @@ class HDFStore(object):
             rootUEP = kwargs.get('rootUEP', None) or self._open_kwargs.get('rootUEP', '/')
             filters = kwargs.get('filters', None) or self._open_kwargs.get('filters', None)
             self._open_kwargs.update(dict(title=title, rootUEP=rootUEP, filters=filters))
-            if (val != self._mode) & (self._mode is not None):
+            if (val != self._mode):
                 self._mode = str(val)
                 if self.source is not None:
                     self.close_source(force=True)
@@ -105,7 +337,7 @@ class HDFStore(object):
         """
         if self.source is None:
             if type(self.lnpfile) == str:
-                self.source = tables.openFile(self.lnpfile, **self._open_kwargs)
+                self.source = tables.openFile(self.lnpfile, mode=self._mode, **self._open_kwargs)
             elif isinstance(self.lnpfile, tables.file.File):
                 self.source = self.lnpfile
             return True
@@ -154,13 +386,71 @@ class HDFStore(object):
             g = list(s.source.walkGroups(where=where))
         return g
 
-    def __getitem__(self, key):
+    def write(self, data, group='/', tablename=None, append=False, silent=False, header={}, **kwargs):
+        """write -- write a data structure into the source file
+
+        keywords
+        --------
+        data: dict like object
+            structure that contains individual columns
+
+        hd5: tables.file.File
+            opened hdf file
+
+        group: str
+            path to where the table will be created
+
+        tablename: str
+            name of the node for the table
+
+        append: bool
+            if set, attempts to add content at the end of an existing node
+            Only works for Tables (not Arrays)
+
+        silent: bool
+            if set, do not print information content
+
+        header: dictionary like
+            attributes to add to the node
+        """
         with self as s:
-            if key not in s.keys():
-                raise KeyError('No object named {} in this store'.format(key))
-            return s.source.getNode(key)
+            if s._mode[0] == 'r':
+                s.set_mode('a')
+            __write__(data, s.source, group, tablename, append, silent, header, **kwargs)
+
+    def __getitem__(self, key):
+        """__getitem__
+        Returns the node corresponding to the key
+        """
+        with self as s:
+            if key[0] != '/':
+                p = '/' + key
+            else:
+                p = key
+            if p not in s.keys():
+                raise KeyError('No object named {} in this store'.format(p))
+            return s.source.getNode(p)
+
+    def __setitem__(self, key, value):
+        """__setitem__
+        create a node with the key path/name and set value as its content
+        """
+        with self as s:
+            if key in s.keys():
+                raise KeyError('Object named {} already exists in this store'.format(key))
+            p = key.split('/')
+            name = p[-1]
+            p = '/'.join(p[:-1])
+            if (p is None) or (p == ''):
+                p = '/'
+            if p[0] != '/':
+                p = '/' + p
+            self.write(value, group=p, tablename=name, silent=True)
 
     def __getattr__(self, name):
+        """__getattr__
+        Give direct access to any function from self.source (tables.file.File)
+        """
         if name in self.__dict__:
             return self.__dict__[name]
         elif hasattr(self.source, name):
@@ -261,3 +551,30 @@ class HDFStore(object):
                             condvars[k] = node.read(field=k)
                 q = eval(expr, condvars, np.__dict__)
         return q
+
+
+def unittest():
+    """unittest
+    Example usage
+    """
+    import numpy as np
+
+    #make a store
+    with HDFStore('tmp.hd5', mode='w') as hd:
+        #make some variables
+        d = {}
+        d['a'] = np.arange(10, dtype=float)
+        d['b'] = np.arange(10, dtype='int')
+        c = np.random.normal(0, 1, (10, 10))
+        d['c'] = c
+
+        #put values into the store
+        hd['/subdir/table'] = d
+        hd['/subdir1/array'] = c
+
+        #check values
+        print hd.keys()
+        print hd['subdir/table']
+        print hd['subdir1/array']
+        hd.removeNode('/subdir1', recursive=True)
+        print hd.keys()
