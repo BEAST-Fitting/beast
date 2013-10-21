2f9b345a56602a798c2cf1810be546ea4c06338a
Morgan Fouesneau <fouesneau@mpia.de>
2013-10-20 23:40:48 -0700

    Added composite class and + operators
    	osl = Kurucz() + Tlusty()
    
    
    Former-commit-id: 359cea9d7fe1240ad0293beabf0f16b55240646f

--- core/stellib.py
+++ core/stellib.py
@@ -15,6 +15,9 @@ from ..external import ezunits
 from ..config import __ROOT__, __NTHREADS__
 from ..include.interp import __interp__
 from ..tools import progressbar
+from matplotlib.path import Path
+from scipy.interpolate import interp1d
+from numpy.lib import recfunctions
 
 
 def isNestedInstance(obj, cl):
@@ -325,7 +328,7 @@ class Stellib(object):
         --------
 
         xypoints: sequence
-            a sequence of N x,y pairs.
+            a sequence of N logg, logT pairs.
 
         dlogT: float
             margin in logT
@@ -339,9 +342,7 @@ class Stellib(object):
             a boolean ndarray, True for points inside the polygon.
             A point on the boundary may be treated as inside or outside.
         """
-        from matplotlib.path import Path
-        xyverts = self.get_boundaries(dlogT=dlogT, dlogg=dlogg, closed=True)
-        p = Path(xyverts)
+        p = self.get_boundaries(dlogT=dlogT, dlogg=dlogg, closed=True)
         return p.contains_points(xypoints)
 
     def get_radius(self, logl, logt):
@@ -415,7 +416,7 @@ class Stellib(object):
         b = leftb + rightb
         if closed:
             b += [b[0]]
-        self._bound = (np.array(b), dlogT, dlogg)
+        self._bound = (Path(np.array(b)), dlogT, dlogg)
         return self._bound[0]
 
     def genQ(self, qname, r, **kwargs):
@@ -543,6 +544,377 @@ class Stellib(object):
                   'name': 'Reinterpolated stellib grid'}
 
         g = SpectralGrid(lamb, seds=specs, grid=Table(_grid), header=header, backend='memory')
+        return g
+
+    def plot_boundary(self, ax, **kwargs):
+        """
+        Valid kwargs are:
+            agg_filter: unknown
+            alpha: float or None
+            animated: [True | False]
+            antialiased or aa: [True | False]  or None for default
+            axes: an :class:`~matplotlib.axes.Axes` instance
+            clip_box: a :class:`matplotlib.transforms.Bbox` instance
+            clip_on: [True | False]
+            clip_path: [ (:class:`~matplotlib.path.Path`,         :class:`~matplotlib.transforms.Transform`) |         :class:`~matplotlib.patches.Patch` | None ]
+            color: matplotlib color spec
+            contains: a callable function
+            edgecolor or ec: mpl color spec, or None for default, or 'none' for no color
+            facecolor or fc: mpl color spec, or None for default, or 'none' for no color
+            figure: a :class:`matplotlib.figure.Figure` instance
+            fill: [True | False]
+            gid: an id string
+            hatch: [ '/' | '\\' | '|' | '-' | '+' | 'x' | 'o' | 'O' | '.' | '*' ]
+            label: string or anything printable with '%s' conversion.
+            linestyle or ls: ['solid' | 'dashed' | 'dashdot' | 'dotted']
+            linewidth or lw: float or None for default
+            lod: [True | False]
+            path_effects: unknown
+            picker: [None|float|boolean|callable]
+            rasterized: [True | False | None]
+            snap: unknown
+            transform: :class:`~matplotlib.transforms.Transform` instance
+            url: a url string
+            visible: [True | False]
+            zorder: any number
+
+        .. seealso::
+
+            :class:`Patch`
+                For additional kwargs
+        """
+        import matplotlib.patches as patches
+        p = self.get_boundaries(dlogT=0.1, dlogg=0.3, closed=True)
+        ax.add_patch(patches.PathPatch(**kwargs))
+        return p
+
+    def __add__(self, other):
+        if not isNestedInstance(other, Stellib):
+            raise ValueError('expecting a Stellib object, got {}'.format(type(other)))
+
+        return CompositeStellib([self, other])
+
+
+class CompositeStellib(Stellib):
+    def __init__(self, osllist, *args, **kwargs):
+        self._olist = osllist
+
+    def __add__(self, other):
+        if not isNestedInstance(other, Stellib):
+            raise ValueError('expecting a Stellib object, got {}'.format(type(other)))
+
+        lst = [k for k in self._olist] + [other]
+        return CompositeStellib(lst)
+
+    def __radd__(self, other):
+        if not isNestedInstance(other, Stellib):
+            raise ValueError('expecting a Stellib object, got {}'.format(type(other)))
+
+        lst = [other] + [k for k in self._olist]
+        return CompositeStellib(lst)
+
+    @property
+    def wavelength(self):
+        """ return a common wavelength sampling to all libraries. This can be
+        used to reinterpolate any spectrum onto a common definition """
+
+        lambs = np.unique([ osl.wavelength for osl in self._olist ])
+        return lambs
+
+    @property
+    def source(self):
+        return ' + '.join([k.name for k in self._olist])
+
+    def which_osl(self, xypoints, dlogT=0.1, dlogg=0.3):
+        """
+        Returns the first library indice that contains each point in xypoints
+
+        keywords
+        --------
+
+        xypoints: sequence
+            a sequence of N logg, logT pairs.
+
+        dlogT: float
+            margin in logT
+
+        dlogg: float
+            margin in logg
+
+        returns
+        -------
+        r: ndarray(dtype=int)
+            a ndarray, 0 meaning no library covers the point, and 1, ... n, for the n-th library
+        """
+        xy = np.asarray(xypoints)
+        res = np.zeros(len(xy), dtype=int)
+        res[self._olist[0].point_inside(xy, dlotT=dlogT, dlogg=dlogg)] = 1
+
+        for ek, ok in enumerate(self._olist[1:]):
+            if 0 in res:
+                ind = np.squeeze(np.where(res == 0))
+                r = ok.point_inside(xy[ind], dlotT=dlogT, dlogg=dlogg)
+                res[ind[r]] = ek
+        return r
+
+    def __repr__(self):
+        return "CompositeStellib, {}\n{}".format(object.__repr__(self), [k.name for k in self._olist])
+
+    def get_boundaries(self, dlogT=0.1, dlogg=0.3, closed=True):
+        """ Returns the closed boundary polygon around the stellar library with
+        given margins
+
+        keywords
+        --------
+        s: Stellib
+            Stellar library object
+
+        dlogT: float
+            margin in logT
+
+        dlogg: float
+            margin in logg
+
+        closed: bool
+            if set, close the polygon
+
+        returns
+        -------
+        b: ndarray[float, ndim=2]
+            (closed) boundary points: [logg, Teff]
+
+        Note
+        ----
+        as computing the boundary could take time, it is saved in the object
+        and only recomputed when parameters are updated
+        """
+        if getattr(self, '_bound', None) is not None:
+            if ((self._bound[1] - dlogT) < 1e-3) and (abs(self._bound[2] - dlogg) < 1e-3):
+                return self._bound[0]
+
+        b = [osl.get_boundaries(dlogT=dlogT, dlogg=dlogg, closed=closed) for osl in self._olist]
+        self._bound = (Path.make_compound_path(*b), dlogT, dlogg)
+        return self._bound[0]
+
+    def interp(self, T0, g0, Z0, L0, dT_max=0.1, eps=1e-6):
+        """ Interpolation of the T,g grid
+
+        Interpolate on the grid and returns star indices and
+        associated weights, and Z.
+        3 to 12 stars are returned.
+        It calls _interp_, but reduce the output to the relevant stars.
+
+        keywords
+        --------
+        T0  double
+            log(Teff) to obtain
+
+        g0  double
+            log(g) to obtain
+
+        T   double
+            log(Teff) of the grid
+
+        g   double
+            log(g) of the grid
+
+        dT_max: float
+            If, T2 (resp. T1) is too far from T compared to T1 (resp. T2),
+            i2 (resp. i1) is not used.
+            (see below for namings)
+
+        eps: foat
+            temperature sensitivity under which points are considered to
+            have the same temperature
+
+        returns
+        -------
+        (osl, r): tuple
+            osl: is the library index starting from 1. 0 means no coverage.
+            r: is the result from interp call on the corresponding library.
+                a 3 to 12 star indexes and associated weights
+        """
+        osl_index = self.which_osl(np.atleast_2d([g0, T0]))[0]
+
+        if osl_index > 0:
+            return (osl_index, self._olist[osl_index - 1].interp(self, T0, g0, Z0, L0, dT_max=0.1, eps=1e-6))
+        else:
+            return [(0, None)]
+
+    def interpMany(self, T0, g0, Z0, L0, dT_max=0.1, eps=1e-6, weights=None, pool=None, nthreads=__NTHREADS__):
+        """ run interp on a list of inputs and returns reduced results
+
+        Interpolation of the T,g grid at Z0 metallicity
+
+        Interpolate on the grid and returns star indices and
+        associated weights, and Z.
+        3 to 12 stars are returned.
+        It calls _interp_, but reduce the output to the relevant stars.
+
+        keywords
+        --------
+        T0  ndarray(float)
+            log(Teff) to obtain
+
+        g0  ndarray(float)
+            log(g) to obtain
+
+        Z0 ndarray(float)
+            metallicity values
+
+        L0 ndarray(float)
+            luminosity values
+
+        dT_max: float
+            If, T2 (resp. T1) is too far from T compared to T1 (resp. T2),
+            i2 (resp. i1) is not used.
+            (see below for namings)
+
+        eps: foat
+            temperature sensitivity under which points are considered to
+            have the same temperature
+
+        weights: ndarray(float)
+            luminosity weigths to apply after interpolation
+
+        pool: Pool-like object
+            specify a multiprocessing pool for parallel processing
+
+        nthreads: int
+            number of processes to use by default
+
+        returns
+        -------
+        returns
+        -------
+        (osl, r): tuple
+            osl: is the library index starting from 1. 0 means no coverage.
+            r: is the result from interp call on the corresponding library.
+                a 3 to 12 star indexes and associated weights
+        """
+        osl_index = self.which_osl(zip(g0, T0))
+
+        g = []
+        for oslk, osl in enumerate(self._olist):
+            # make a generator to avoid keeping all in memory
+            ind = np.where(osl_index - 1 == oslk)
+            g.append( oslk + 1, osl.interpMany(T0[ind], g0[ind], Z0[ind], L0[ind], dT_max=dT_max, eps=eps, weights=weights, pool=pool, nthreads=nthreads) )
+
+        return g
+
+    def genQ(self, qname, r, **kwargs):
+        """ Generate a composite value from a previously calculated
+            interpolation
+            Works on 1 desired star or a population of stars
+
+        keywords
+        --------
+        qname: str
+            quantity name from self.grid
+
+        r: (osl, r) tuple
+            osl: is the library index starting from 1. 0 means no coverage.
+            r: is the result from interp call on the corresponding library.
+
+        returns
+        -------
+        q: float
+            value (from weighted sum)
+        """
+        vals = 0.
+        for _osl, _r in r:
+            if _osl > 0:
+                vals += self._olist[_osl - 1].genQ(qname, _r, **kwargs)
+        return vals
+
+    def genSpectrum(self, T0, g0=None, Z0=None, weights=None, **kwargs):
+        """ Generate a composite sprectrum
+            Does the interpolation or uses a previously calculated interpolation
+            Works on 1 desired star or a population of stars
+
+        keywords
+        --------
+        T0  ndarray(float)
+            log(Teff) to obtain
+
+        g0  ndarray(float)
+            log(g) to obtain
+
+        Z0 ndarray(float)
+            metallicity values
+
+        weights: ndarray(float)
+            individual weights of each star
+
+        **kwargs forwarded to interp(Many)
+
+        Note: if T0 and g0 are iterable, it calls interpMany
+
+        returns
+        -------
+        s: ndarray
+            an array containing the composite spectrum reinterpolated onto self.wavelength
+        """
+        if Z0 is not None:
+            if hasattr(T0, '__iter__'):
+                _r = self.interpMany(T0, g0, Z0, weights=weights, **kwargs)
+            else:
+                _r = np.asarray(self.interp(T0, g0, Z0, weight=weights, **kwargs))
+        else:
+            _r = T0
+
+        l0 = self.wavelength
+        s = np.zeros(len(l0), dtype=float)
+        for osl, _rk in _r:
+            if osl > 0:
+                sp = (((self._olist[osl - 1].spectra[_r[:, 0].astype(int)].T) * _r[:, 1]) ).sum(1)
+                lamb = self._olist[osl - 1].wavelength
+                s += np.interp(l0, lamb, sp)
+        return s
+
+    def gen_spectral_grid_from_given_points(self, pts, bounds=dict(dlogT=0.1, dlogg=0.3)):
+        """ Reinterpolate a given stellar spectral library on to an Isochrone grid
+
+        keywords
+        --------
+        pts: dict like structure of points
+            dictionary like or named data structure of points to interpolate at.
+            must contain:
+                * logg  surface gravity in log-scale
+                * logT  log of effective temperatures (in Kelvins)
+                * logL  log of luminosity in Lsun units
+                * Z     metallicity
+
+        bounds:  dict
+            sensitivity to extrapolation (see grid.get_stellib_boundaries)
+            default: {dlogT:0.1, dlogg:0.3}
+
+        Returns
+        -------
+        g: SpectralGrid
+            Spectral grid (in memory) containing the requested list of stars and associated spectra
+        """
+        osl_index = self.which_osl(zip(pts['logg'], pts['logT']))
+
+        seds = []
+        grid = []
+        l0 = self.wavelength
+        for oslk, osl in enumerate(self._olist):
+            # make a generator to avoid keeping all in memory
+            _pts = ( (logg, logT) for (logg, logT, ok) in zip(pts['logg'], pts['logT'], osl_index) if (ok - 1 == oslk) )
+            gk = osl.gen_spectral_grid_from_given_points(_pts, bounds=dict(dlogT=0.1, dlogg=0.3))
+            gk.seds = interp1d(gk.lamb, gk.seds, axis=1)(l0)
+            seds.append(gk.seds)
+            grid.append(gk.grid)
+
+        header = {'stellib': self.source,
+                  'comment': 'radius in Rsun',
+                  'name': 'Reinterpolated stellib grid'}
+
+        _grid = recfunctions.stack_arrays( grid, defaults=None, usemask=False, asrecarray=True)
+
+        g = SpectralGrid(l0, seds=np.vstack(seds), grid=Table(_grid), header=header, backend='memory')
+
         return g
 
 
