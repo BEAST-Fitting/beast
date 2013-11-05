0a09c518abdb89fbc704e3d54eb08c1e9980aaf9
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2013-11-04 16:31:34 -0800

    debug
    Former-commit-id: fd4afae0c0a1b620320a86c7c4750f95e5971c21

--- core/ezpadova/cmd.py
+++ core/ezpadova/cmd.py
@@ -36,7 +36,7 @@ import urllib
 import urllib2
 import zlib
 import re
-from eztables import Table
+from ...external.eztables import Table
 from StringIO import StringIO
 
 # internal parameters
@@ -208,16 +208,12 @@ __def_args__ = {'binary_frac': 0.3,
                 'submit_form': 'Submit'}
 
 
-
 def file_type(filename, stream=False):
     """ Detect potential compressed file
     Returns the gz, bz2 or zip if a compression is detected, else None.
     """
-    magic_dict = {
-        "\x1f\x8b\x08": "gz",
-        "\x42\x5a\x68": "bz2",
-        "\x50\x4b\x03\x04": "zip"
-        }
+    magic_dict = { "\x1f\x8b\x08": "gz", "\x42\x5a\x68": "bz2", "\x50\x4b\x03\x04": "zip" }
+
     max_len = max(len(x) for x in magic_dict)
     if not stream:
         with open(filename) as f:
