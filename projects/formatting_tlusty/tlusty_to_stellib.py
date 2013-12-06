5f58e7e83c9e73d53b3f12e7da48ab624a0e9dee
Kirill Tchernyshyov <ktchernyshyov@pha.jhu.edu>
2013-12-06 15:23:56 -0500

    Units and factors of pi.
    Former-commit-id: 4ed5588c548ed1c6f7ee8dd84cf2f32eb642b3bc

--- projects/formatting_tlusty/tlusty_to_stellib.py
+++ projects/formatting_tlusty/tlusty_to_stellib.py
@@ -17,8 +17,8 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 # TLUSTY atmospheres.
 #
 path_to_tlusty_models = '/Users/K/Documents/PHAT_SED/Stellar_Atmospheres/TLUSTY' 
-out_path = 'sedfitter/libs/tlusty.subgrid.fits'
-filepaths = glob.glob(path_to_tlusty_models+'/*STAR*/Gmodels/*.flux')
+out_path = 'sedfitter/libs/tlusty.grid.fits'
+filepaths = glob.glob(path_to_tlusty_models+'/*STAR*/*models/*.flux')
 
 def edges_from_centers(centers):
   length = centers.size
@@ -37,19 +37,25 @@ def sanitize_tlusty_input(frequency, flux):
   # This isn't in any way correct, but is a fairly small effect, especially
   # considering the coarseness of the Kurucz wavelength grid relative to the
   # TLUSTY one. 
-   
-  duplicates = np.where(frequency[1:]==frequency[:-1])[0]
-  for i in range(len(duplicates)):
-    frequency[duplicates[i]+1] = 0.5*(frequency[duplicates[i]+1] + frequency[duplicates[i]+2])
   order = np.argsort(frequency)
-  return frequency[order], flux[order]
+  frequency = frequency[order]
+  flux = flux[order]
+  duplicates = np.where(frequency[1:]==frequency[:-1])[0]
+  duplicate_frequencies = np.unique(frequency[duplicates])
+  duplicate_sequences = [np.where(frequency==freq)[0] for freq in duplicate_frequencies]
+  for duplicate_sequence in duplicate_sequences:
+    frequency[duplicate_sequence] = np.linspace(frequency[duplicate_sequence[0]], 
+                                                frequency[duplicate_sequence[-1]+1],
+                                                len(duplicate_sequence), 
+                                                endpoint=False)
+  return frequency, flux
 
 kurucz = stellib.Kurucz()
 c = 2.998e18 #in Angstroms per second
 ck_waves = kurucz.wavelength
 ck_reversed_frequencies = c/ck_waves
-ck_frequencies = ck_reversed_frequencies[::-1]
-ck_frequency_bounds = edges_from_centers(ck_frequencies)
+#ck_frequencies = ck_reversed_frequencies[::-1]
+#ck_frequency_bounds = edges_from_centers(ck_frequencies)
 ck_wave_bounds = edges_from_centers(ck_waves)
 ck_wave_spans = ck_wave_bounds[1:]-ck_wave_bounds[:-1]
 
@@ -72,11 +78,16 @@ with progressbar.PBar(len(filepaths), txt='Reading %d files' %(len(filepaths))) 
     tl_frequencies = tl_fluxes_and_frequencies[::2][::-1]
     tl_fluxes = tl_fluxes_and_frequencies[1::2][::-1]
     tl_frequencies, tl_fluxes = sanitize_tlusty_input(tl_frequencies, tl_fluxes)
-    spl = InterpolatedUnivariateSpline(tl_frequencies, tl_fluxes, k=1)
+    tl_wavelengths = c/tl_frequencies[::-1]
+    tl_fluxes = tl_fluxes[::-1] * (c/(tl_wavelengths*tl_wavelengths))
+    #finite_sel = np.where(np.isfinite(tl_fluxes))
+    #tl_fluxes = tl_fluxes[finite_sel]
+    #tl_wavelengths = tl_wavelengths[finite_sel]
+    spl = InterpolatedUnivariateSpline(tl_wavelengths, tl_fluxes, k=1)
     vect_int = np.vectorize(spl.integral)
-    reversed_integrated_flux = vect_int(ck_frequency_bounds[:-1], ck_frequency_bounds[1:])
-    flux_per_wavelength = reversed_integrated_flux[::-1]/(ck_wave_spans[::-1])
-    spectrum[e] = flux_per_wavelength
+    integrated_flux = vect_int(ck_wave_bounds[:-1], ck_wave_bounds[1:])
+    flux_per_wavelength = integrated_flux/ck_wave_spans
+    spectrum[e] = flux_per_wavelength*4*np.pi
     m = p.findall(filepath)[0]
     Z_vals[e] = Z_dict[m[0]]
     T_eff_vals[e] = np.float(m[1])
