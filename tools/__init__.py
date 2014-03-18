02ee40aa0ee7806876e8eeae4b82a2e635d6a537
Morgan Fouesneau <morgan.fouesneau@gmail.com>
2014-03-18 14:35:39 -0700

    Updated details
    Former-commit-id: 421390897cdee49dc804371c93f0a680f62217fb

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,4 +3,4 @@ from . import progressbar
 from . import contextManagers
 from . import ezpipeline
 from . import pbar
-__all__ = 'decorators progressbar contextManagers ezpipeline binningAxis pbar'.split()
+__all__ = 'decorators progressbar contextManagers ezpipeline binningAxis pbar helpers'.split()
