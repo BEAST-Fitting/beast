375737717c1d0b16128fed21bb8902cb1485fae5
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2014-03-30 14:09:56 -0700

    Added many more functions
    Former-commit-id: 9d06093e9f272f6f2c7e48a9fbc7e58c81266ec5

--- tools/helpers.py
+++ tools/helpers.py
@@ -1,7 +1,108 @@
-""" some common tools """
+"""
+This is a first collection of tools making the design easier
+"""
 import sys
+from functools import partial, wraps, update_wrapper
+from inspect import getargspec, ismethod
+import warnings
+import numpy as np
+import itertools
 
-__all__ = ['isNestedInstance', 'path_of_module', 'pretty_size_print', 'nbytes']
+from ezunits import unit, hasUnit
+
+#replace the common range by the generator
+try:
+    range = xrange
+except NameError:
+    pass
+
+
+__all__ = ['NameSpace', 'Pipe', 'Pipeable', 'Pipegroup', 'chunks',
+           'deprecated', 'generator', 'isNestedInstance', 'keywords_first',
+           'kfpartial', 'merge_records', 'missing_units_warning', 'nbytes',
+           'path_of_module', 'pretty_size_print', 'type_checker',
+           'val_in_unit']
+
+
+class NameSpace(dict):
+    """A dict subclass that exposes its items as attributes.
+    """
+    def __init__(self, name, *args, **kwargs):
+        self.__name__ = name
+        dict.__init__(self, *args, **kwargs)
+
+    def __dir__(self):
+        return tuple(self)
+
+    def __repr__(self):
+        names = ', '.join([k for k in dir(self) if k[0] != '_'])
+        return "{s.__name__:s}: {r:s}".format(s=self, r=names)
+
+    def __getattribute__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            msg = "'{s.__name__:s}' has no attribute '{name:s}'"
+            raise AttributeError(msg.format(s=self, name=name))
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        return False
+
+
+def generator(func):
+    """ A dummy decorator that only make codes mode readable.
+    It allow to explicitly mark a function as generator (yielding values)
+    and does nothing more than calling the initial function
+    """
+    @wraps(func)
+    def deco(*args, **kwargs):
+        return func(*args, **kwargs)
+    return deco
+
+
+def deprecated(func):
+    """ A dummy decorator that warns against using a deprecated function """
+    @wraps(func)
+    def deco(*args, **kwargs):
+        txt = 'Function {0:s} is deprecated. You should avoid its usage'
+        warnings.warn(txt.format(func.__name__))
+        return func(*args, **kwargs)
+    return deco
+
+
+@generator
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+
+    Parameters
+    ----------
+    l: iterable
+        object to iter over
+
+    n: int
+        number of elements per slice
+
+    Returns
+    -------
+    chunk: tuple
+        n values from l
+    """
+    it = iter(l)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if chunk:
+            yield chunk
+        else:
+            raise StopIteration
 
 
 def isNestedInstance(obj, cl):
@@ -21,11 +122,271 @@ def isNestedInstance(obj, cl):
     r: bool
         True if obj is indeed an instance or subclass instance of cl
     """
-    tree = []
-    for k in cl.__subclasses__():
-        tree += k.__subclasses__()
-    tree += cl.__subclasses__() + [ cl ]
+    tree = [ cl ]
+    if hasattr(cl, '__subclasses'):
+        for k in cl.__subclasses():
+            if hasattr(k, '__subclasses'):
+                tree += k.__subclasses__()
     return issubclass(obj.__class__, tuple(tree))
+
+
+def type_checker(name, obj, tp):
+    """ Check a given type and raise a type error if not correct
+
+    Parameters
+    ----------
+    name: str
+        name of the variable to show in the exception text
+
+    obj: object
+        object to check
+
+    tp: type
+        expected type of obj
+
+    Raises
+    ------
+    :exc:TypeError:
+        raises a TypeError if object is not of the correct type of a subclass of it
+    """
+    if not isNestedInstance(obj, tp):
+        txt = 'Expected "{0:s}" of type {1:s}, got {2:s} instead.'
+        raise TypeError(txt.format(name, str(tp.__name__), str(type(obj).__name__)))
+
+
+class Pipeable(object):
+    """ Decorator overloading | operator (__ror__) such that you can pipe
+    functions where the first argument is the variable on the left side of the
+    | operator.
+    This decorator allows you to use the decorated function normally and uses
+    the provided values when using in pipes.
+
+    >>> import pylab as plt
+    >>> _p = Pipeable(plt.plot, color='red', linestyle='--')
+    >>> _p(range(10), 'o-')  #  works
+    >>> range(10) | _p      #  will plot a red dashed line
+
+    """
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def __ror__(self, lhs):
+        return self.func(lhs, *self.args, **self.kwargs)
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __repr__(self):
+        return self.func.__repr__()
+
+
+class Pipe(object):
+    """ Decorator overloading | operator (__ror__) such that you can pipe
+    functions where the first argument is the variable on the left side of the
+    | operator.
+    The difference with Pipeable is that you cannot use decorated function
+    outside of pipes but you gain the possibility to update the calling
+    parameters
+
+    Used with keywords_first make this a powerful Task
+    """
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        update_wrapper(self, func)
+
+    def __or__(self, other):
+        if isinstance(other, Pipe):
+            return Pipegroup( (self, other), mode='multi' )
+
+    def __and__(self, other):
+        if isinstance(other, Pipe):
+            return Pipegroup( (self, other) )
+
+    def __ror__(self, other):
+            return self.func(other, *self.args, **self.kwargs)
+
+    def __call__(self, *args, **kwargs):
+        return Pipeable(self.func, *args, **kwargs)
+
+    def __repr__(self):
+        return 'Pipe: {}'.format(self.func.__repr__())
+
+    def __str__(self):
+        return '{}'.format(self.func.__name__)
+
+
+class Pipegroup(object):
+    def __init__(self, pipes, mode='sequential'):
+        self.pipes = list(pipes)
+        self.mode = mode
+        self.func = self
+
+    def __len__(self):
+        return len(self.pipes)
+
+    def seq_call(self, val, *args, **kwargs):
+        r = self.pipes[0].func(val)
+        if len(self) > 1:
+            for pk in self.pipes[1:]:
+                r = pk.func(r)
+        return r
+
+    def append(self, other):
+        self.pipes.append(other)
+
+    def __or__(self, other):
+        if isinstance(other, Pipe):
+            if self.mode in ['multi', '|']:
+                self.append(other)
+                return self
+            else:
+                return Pipegroup( (self, other), mode='multi')
+
+    def __and__(self, other):
+        if isinstance(other, Pipe):
+            if self.mode in ['sequential', '&']:
+                self.append(other)
+                return self
+            else:
+                return Pipegroup( (self, other), mode='sequential')
+
+    def multi_call(self, vals, iter=True):
+        return [ pk.func(vals) for pk in self.pipes ]
+
+    def __call__(self, val, *args, **kwargs):
+        mode = kwargs.get('mode', self.mode).lower()
+        if mode in ['sequential', '&']:
+            return self.seq_call(val, *args, **kwargs)
+        elif mode in ['multi', '|']:
+            return self.multi_call(val, *args, **kwargs)
+        else:
+            raise NotImplemented
+
+    def __ror__(self, other):
+        return self(other)
+
+    def __repr__(self):
+        txt = 'Pipegroup: mode={},\n\t | '.format(self.mode)
+
+        if self.mode == 'sequential':
+            txt += ' & '.join([str(pk) for pk in self.pipes])
+        else:
+            txt += '\n\t | '.join([str(pk) for pk in self.pipes])
+        return txt
+
+    def __str__(self):
+        if self.mode == 'sequential':
+            delim = ' & '
+        else:
+            delim = ' | '
+        return '({})'.format(delim.join([str(pk) for pk in self.pipes])).replace('Pipe: ', '')
+
+
+def keywords_first(f):
+    """ Decorator that enables to access any argument or keyword as a keyword """
+    ## http://code.activestate.com/recipes/577922/ (r2)
+    @wraps(f)
+    def wrapper(*a, **k):
+        a = list(a)
+        #for idx, arg in enumerate(f.func_code.co_varnames[:f.func_code.co_argcount], -ismethod(f)):
+        for idx, arg in enumerate(getargspec(f).args, -ismethod(f)):  # or [0] in 2.5
+            if arg in k:
+                if idx < len(a):
+                    a.insert(idx, k.pop(arg))
+                else:
+                    break
+        return f(*a, **k)
+    return wrapper
+
+
+def kfpartial(fun, *args, **kwargs):
+    """ Allows to create partial functions with arbitrary arguments/keywords """
+    return partial(keywords_first(fun), *args, **kwargs)
+
+
+def missing_units_warning(name, defaultunit):
+    """ Warn if any unit is missing
+
+    Paramaters
+    ----------
+    name: str
+        name of the variable
+
+    defaultunit: str
+        default unit definition
+
+    Raises
+    ------
+    warning: warnings.warn
+        warn if units are assumed
+    """
+    warnings.warn('Variable {0:s} does not have explicit units. Assuming `{1:s}`'.format(name, defaultunit))
+
+
+def val_in_unit(varname, value, defaultunit):
+    """ check units and convert to defaultunit or create the unit information
+
+    Parameters
+    ----------
+    varname: str
+        name of the variable
+
+    value: value
+        value of the variable, which may be unitless
+
+    defaultunit: str
+        default units is unitless
+
+    Returns
+    -------
+    quantity: ezunits.Quantity
+        value with units
+
+    Example
+    -------
+    >>> r = 0.5
+    >>> print(val_in_unit('r', r, 'degree'))
+    # UserWarning: Variable r does not have explicit units. Assuming `degree`
+    <Quantity(0.5, 'degree')>
+
+    >>> r = 0.5 * unit['degree']
+    >>> print(val_in_unit('r', r, 'degree'))
+    <Quantity(0.5, 'degree')>
+    """
+    if not hasUnit(value):
+        missing_units_warning(varname, defaultunit)
+        return value * unit[defaultunit]
+    else:
+        return value.to(defaultunit)
+
+
+def merge_records(lst):
+    """ generates a stack of records even with slightly different but compatible dtypes
+
+    Parameters
+    ----------
+
+    lst: sequence of np.recarray
+        sequence of individual records
+
+    Returns
+    -------
+    val: np.recarray
+        array of stacked records
+        Note if if lst is empty, returns an empty list
+    """
+    r = []
+    for rk in lst:
+        r.append(rk.tolist()[0])
+    names = rk.dtype.names
+    if len(r) > 0:
+        return np.rec.fromrecords(r, names=names)
+    else:
+        return []
 
 
 def path_of_module(mod=None):
