8697207efd34c7ce042389544c8aa3efeb407f64
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2014-04-08 09:58:52 -0700

    Added keyword in merge function
    Former-commit-id: aadf2216d5c0d2317b615242ed10a8f52ce62122

--- projects/morgan/mf_phat_b12_para.py
+++ projects/morgan/mf_phat_b12_para.py
@@ -10,7 +10,7 @@ def prepare_input_chunks():
     return datamod.prepare_individual_inputs(datamod.obsfile, 14000)
 
 
-def merge_inputs_outputs():
+def merge_inputs_outputs(project=None):
     import mf_phat_b12 as datamod
     import glob
     from beast.external.eztables import Table
@@ -24,7 +24,10 @@ def merge_inputs_outputs():
     if len(lst) == 0:
         raise ValueError('cannot find any chunk. Did you run prepare_individual_inputs?')
 
-    outname = datamod.project[:]
+    if project is None:
+        outname = datamod.project[:]
+    else:
+        outname = project[:]
 
     t_final = None
     for chunk, input_fname in Pbar(len(lst)).iterover(enumerate(lst)):
