8dfa721a3a5d0cb0ec502689472e3ffc27b1c209
Morgan Fouesneau <fouesneau@mpia.de>
2014-08-05 09:58:53 +0200

    Corrected toothpick noise model incl. mag cut
    Former-commit-id: a659afa8a9abf5f7b94deec7436df938f2b7f125

--- core/noisemodel/toothpick.py
+++ core/noisemodel/toothpick.py
@@ -1,3 +1,26 @@
+"""
+Toothpick noise model assumes that every photometric band is independent from the others.
+
+The following package implements two classes that corresponds to two variants of input AST data.
+
+the first :class:`MultiFilterASTs` assumes that all AST information is compiled
+into on single table, in which one entry corresponds to one artificial star and recovered values
+
+the second :class:`perCameraASTs` assumes that the information is split into
+multiple tables, and implements the equivalent of multiple instances of
+:class:`MultiFilterASTs` in parallel to calculate the model.
+
+Method
+------
+Statistics are computed for each input artificial star.  For each input star,
+we find the k-NN in input magnitude space and compute their mean and variance
+also in magnitude and completeness fractions.
+
+Finally, we return the flux conversion of the statistics: bias and stddev
+dispersion per input star averaged over k-NN.
+
+TODO: +++ not recovered if delta_mag > 0.8 (factor of 2 in flux)
+"""
 import numpy as np
 
 from .noisemodel import NoiseModel
@@ -8,11 +31,22 @@ from ...tools.pbar import Pbar
 
 
 class MultiFilterASTs(NoiseModel):
+    """ Implement a noise model for which input information of ASTs are
+    provided as one single table
 
+    Attributes
+    ----------
+    astfile: str
+        file containing the ASTs
+
+    filters: sequence(str)
+        sequence of filter names
+    """
     def __init__(self, astfile, filters, *args, **kwargs):
         NoiseModel.__init__(self, astfile, *args, **kwargs)
         self.setFilters(filters)
-        self.set_data_mappings()
+        if not 'pass_mapping' in kwargs:
+            self.set_data_mappings()
 
         self._fluxes = None
         self._biases = None
@@ -36,7 +70,8 @@ class MultiFilterASTs(NoiseModel):
             print('Warning: Mapping failed. This could lead to wrong results')
 
     def _compute_stddev(self, mag_in, mag_out, k=10, eps=0,
-                        completeness_mag_cut=80, name_prefix=None):
+                        completeness_mag_cut=80, name_prefix=None,
+                        asarray=False):
         """
         Computes standard deviation and store the result in a dictionary
 
@@ -61,10 +96,13 @@ class MultiFilterASTs(NoiseModel):
             if set, all output names in the final structure will start with this
             prefix.
 
+        asarray: bool
+            if set returns a structured ndarray instead of a dictionary
+
         Returns
         -------
-        d: np.recarray
-            named array containing the statistics
+        d: dict or np.recarray
+            dictionary or named array containing the statistics
 
 
         Method
@@ -75,7 +113,6 @@ class MultiFilterASTs(NoiseModel):
 
         Finally, we return the flux conversion of the statistics: bias and stddev
         dispersion per input star averaged over k-NN.
-
         """
         n_asts = len(mag_in)
 
@@ -91,11 +128,18 @@ class MultiFilterASTs(NoiseModel):
         std_mag = np.zeros(n_asts, dtype=float)
         completeness = np.zeros(n_asts, dtype=float)
 
+        norm_std = float( 1. / np.sqrt( float(k) - 1.))
         for i in range(n_asts):
             nn_i = mag_out[NN_mag[i]]
-            std_mag[i] = np.sqrt( (1. / k) * ( (nn_i - mag_in[i]) ** 2).sum(axis=0))
-            completeness[i] = np.sum(nn_i < completeness_mag_cut, axis=0)
+            # only recovered stars are considered
+            # recovered =      stars < completeness_mag_cut
+            #             and  stars with |flux_out / flux_in| < 2
+            #                         <=> |mag_out - mag_in| < 0.76 (0.756...)
+            ind = (nn_i < completeness_mag_cut) & (np.abs(nn_i - mag_in[NN_mag[i]] < 0.76))
+            std_mag[i] = np.sqrt(( (nn_i[ind] - mag_in[i]) ** 2).sum(axis=0))
+            completeness[i] = np.sum(ind)
 
+        std_mag *= norm_std
         completeness /= float(k)
 
         mag_inup = mag_in + std_mag
@@ -117,7 +161,10 @@ class MultiFilterASTs(NoiseModel):
              name_prefix + 'FLUX_OUT': f_out,
              name_prefix + 'COMPLETENESS': completeness}
 
-        return convert_dict_to_structured_ndarray(d)
+        if asarray:
+            return convert_dict_to_structured_ndarray(d)
+        else:
+            return d
 
     def fit(self, k=10, eps=0, completeness_mag_cut=80, progress=True):
         """
@@ -160,9 +207,9 @@ class MultiFilterASTs(NoiseModel):
             d = self._compute_stddev(mag_in, mag_out, k=k, eps=eps,
                                      completeness_mag_cut=completeness_mag_cut)
 
-            self._fluxes[:, e] = d['FLUX_IN'] / self.vega_flux[e]
-            self._sigmas[:, e] = d['FLUX_STD'] / self.vega_flux[e]
-            self._biases[:, e] = d['FLUX_BIAS'] / self.vega_flux[e]
+            self._fluxes[:, e] = d['FLUX_IN'] * self.vega_flux[e]
+            self._sigmas[:, e] = d['FLUX_STD'] * self.vega_flux[e]
+            self._biases[:, e] = d['FLUX_BIAS'] * self.vega_flux[e]
             self._compls[:, e] = d['COMPLETENESS']
             del d
 
@@ -238,13 +285,22 @@ class MultiFilterASTs(NoiseModel):
         return self.interpolate(sedgrid, **kwargs)
 
 
-class perCamereASTs(NoiseModel):
+class perCameraASTs(NoiseModel):
+    """ Implement a noise model for which input information of ASTs are
+    provided as multiple tables
+
+    Attributes
+    ----------
+    astfiles: sequence(str)
+        files containing the ASTs
+
+    filters: sequence(sequence(str))
+        sequence of sequence of filter names (one per astfile)
+    """
 
     def __init__(self, astfiles, filters, *args, **kwargs):
-
-        NoiseModel.__init__(self, astfiles, *args, **kwargs)
-        self.models = [ MultiFilterASTs(astfile) for astfile in astfiles ]
-        self.setFilters(filters)
+        self.models = [ MultiFilterASTs(astfile, filts, pass_mapping=True) for astfile, filts in zip(astfiles, filters) ]
+        self.set_data_mappings()
 
     def set_data_mappings(self):
         """ hard code mapping directly with the interface to PHAT-like ASTs
@@ -254,7 +310,15 @@ class perCamereASTs(NoiseModel):
             it makes it trivial to update this function for other input formats
         """
         #TODO: update the mapping to stick to the initial PHAT version
-        pass
+        for filts, model in zip(self.filters, self.models):
+            for k in filts:
+                try:
+                    #self.data.set_alias(k, k.split('_')[-1].lower() + '_rate')
+                    model.data.set_alias(k + '_out', k.split('_')[-1].upper() + '_VEGA')
+                    model.data.set_alias(k + '_in', k.split('_')[-1].upper() + '_IN')
+                except Exception as e:
+                    print('Warning: Mapping failed. This could lead to wrong results')
+                    raise e
 
     def fit(self, k=10, eps=0, completeness_mag_cut=80, progress=True):
         """
@@ -288,18 +352,26 @@ class perCamereASTs(NoiseModel):
 
         Parameters
         ----------
-        filters: sequence
-            list of filters using the internally normalized namings
+        filters: sequence or sequences
+            list of filters per camera using the internally normalized namings
         """
         self.filters = filters
-        for model in self.models:
-            model.setFilters(filters)
+        for model, filts in zip(self.models, self.filters):
+            model.setFilters(filts)
+
+    @property
+    def filters(self):
+        return [model.filters for model in self.models]
+
+    @property
+    def astfile(self):
+        return [model.astfile for model in self.models]
 
     @property
     def vega_flux(self):
         return self.models[0].vega_flux
 
-    def interpolate(self, sedgrid):
+    def interpolate(self, sedgrid, progress=True):
         """
         Interpolate the results of the ASTs on a model grid
 
@@ -319,28 +391,36 @@ class perCamereASTs(NoiseModel):
         comp: ndarray
             completeness table per model
         """
-        raise NotImplemented
         flux = sedgrid.seds
         N, M = flux.shape
 
-        if M != len(self.filters):
+        nfilters = sum([len(model.filters) for model in self.models])
+
+        if M != nfilters:
             raise AttributeError('the grid of models does not seem to be defined with the same number of filters')
 
         bias = np.empty((N, M), dtype=float)
         sigma = np.empty((N, M), dtype=float)
         compl = np.empty((N, M), dtype=float)
 
-        for i in range(M):
+        if progress is True:
+            pbar = Pbar(M, desc='Evaluating model')
 
-            _fluxes = self._fluxes[:, i]
-            arg_sort = np.argsort(self._fluxes[:, i])
-            _fluxes = _fluxes[arg_sort]
+        for j, model in enumerate(self.models):
+            for i in range(len(model.filters)):
 
-            bias[:, i] = np.interp(flux[:, i], _fluxes, self._biases[arg_sort, i] )
-            sigma[:, i] = np.interp(flux[:, i], _fluxes, self._sigmas[arg_sort, i])
-            compl[:, i] = np.interp(flux[:, i], _fluxes, self._compls[arg_sort, i])
+                if progress:
+                    pbar.update(i + j)
 
-        return (bias, sigma, compl)
+                _fluxes = model._fluxes[:, i + j]
+                arg_sort = np.argsort(model._fluxes[:, i + j])
+                _fluxes = _fluxes[arg_sort]
+
+                bias[:, i + j] = np.interp(flux[:, i], _fluxes, model._biases[arg_sort, i] )
+                sigma[:, i + j] = np.interp(flux[:, i], _fluxes, model._sigmas[arg_sort, i])
+                compl[:, i + j] = np.interp(flux[:, i], _fluxes, model._compls[arg_sort, i])
+
+            return (bias, sigma, compl)
 
     def __call__(self, sedgrid, **kwargs):
         return self.interpolate(sedgrid, **kwargs)
