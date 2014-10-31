c559dc01b89a0c90c79249614145bd44be35c391
Karl Gordon <kgordon@stsci.edu>
2014-10-31 12:50:06 -0400

    Updated toothpick model -> computations done in flux units and full noisemodel (e.g. fit function) works
    Former-commit-id: 78bf0aefeefe01a1171122e846c5d719efd02bc1

--- projects/ngc4214/plot_noisemodel.py
+++ projects/ngc4214/plot_noisemodel.py
@@ -41,9 +41,21 @@ if __name__ == '__main__':
     mag_in = model.data[filters[plot_filter_num] + '_in']
     mag_out = model.data[filters[plot_filter_num] + '_out']
 
-    d = model._compute_stddev(mag_in, mag_out, k=10, eps=0, completeness_mag_cut=80)
+    completeness_mag_cut = 80
 
-    d2 = model._compute_stddev_wrong(mag_in, mag_out, k=10, eps=0, completeness_mag_cut=80)
+    d = model._compute_stddev_mag(mag_in, mag_out, k=10, eps=0, completeness_mag_cut=completeness_mag_cut)
+
+    d2 = model._compute_stddev_wrong(mag_in, mag_out, k=10, eps=0, completeness_mag_cut=completeness_mag_cut)
+
+    #good_indxs,= np.where(mag_out < completeness_mag_cut)
+    #flux_in = 10 ** (-0.4*mag_in[good_indxs])
+    #flux_out = 10 ** (-0.4*mag_out[good_indxs])
+
+    d3 = model._compute_stddev(mag_in, mag_out, k=10, eps=0, completeness_mag_cut=completeness_mag_cut)
+
+    flux_out = model.data[filters[plot_filter_num].split('_')[-1].upper() + '_RATE']
+
+    d4 = model._compute_stddev(mag_in, flux_out, k=10, eps=0, completeness_mag_cut=-1)
 
     import matplotlib.pyplot as pyplot
 
@@ -51,26 +63,34 @@ if __name__ == '__main__':
 
     ax[0].plot(d2['FLUX_IN'],d2['COMPLETENESS'],'ro',markerfacecolor='none',markeredgecolor='r')
     ax[0].plot(d['FLUX_IN'],d['COMPLETENESS'],'bo')
+    ax[0].plot(d3['FLUX_IN'],d3['COMPLETENESS'],'go')
+    ax[0].plot(d4['FLUX_IN'],d4['COMPLETENESS'],'co',markerfacecolor='none',markeredgecolor='c')
     ax[0].set_xscale('log')
     ax[0].set_yscale('linear')
     ax[0].set_ylabel('Completeness')
     ax[0].set_ylim(-0.1,1.1)
 
-    ax[1].plot(d2['FLUX_IN'],d2['FLUX_BIAS']/d2['FLUX_IN'],'ro',markerfacecolor='none',markeredgecolor='r',label='old')
-    ax[1].plot(d['FLUX_IN'],d['FLUX_BIAS']/d['FLUX_IN'],'bo',label='new')
+    ax[1].plot(d2['FLUX_IN'],d2['FLUX_BIAS']/d2['FLUX_IN'],'ro',markerfacecolor='none',markeredgecolor='r',label='old(mag)')
+    ax[1].plot(d['FLUX_IN'],d['FLUX_BIAS']/d['FLUX_IN'],'bo',label='new(mag)')
+    ax[1].plot(d3['FLUX_IN'],d3['FLUX_BIAS']/d3['FLUX_IN'],'go',label='newnew(flux)')
+    ax[1].plot(d4['FLUX_IN'],d4['FLUX_BIAS']/d4['FLUX_IN'],'co',markerfacecolor='none',markeredgecolor='c',label='newrate(flux)')
     ax[1].plot([1e-13,1e-6],[0.0,0.0],'g--')
     ax[1].set_xscale('log')
     ax[1].set_yscale('linear')
     ax[1].set_ylabel('Bias/Flux(Input)')
     ax[1].set_ylim(-1.1,1.1)
-    ax[1].legend()
 
     ax[2].plot(d2['FLUX_IN'],d2['FLUX_STD']/d2['FLUX_IN'],'ro',markerfacecolor='none',markeredgecolor='r')
     ax[2].plot(d['FLUX_IN'],d['FLUX_STD']/d['FLUX_IN'],'bo')
+    ax[2].plot(d3['FLUX_IN'],d3['FLUX_STD']/d3['FLUX_IN'],'go')
+    ax[2].plot(d4['FLUX_IN'],d4['FLUX_STD']/d4['FLUX_IN'],'co',markerfacecolor='none',markeredgecolor='c')
     ax[2].set_xscale('log')
     ax[2].set_yscale('log')
-    ax[2].set_xlabel('Flux(Input)')
+    ax[2].set_xlabel('Flux(Input) [Normalized to Vega]')
     ax[2].set_ylabel('Sigma/Flux(Input)')
 
-    pyplot.show()
+    ax[1].legend()
 
+    fig.subplots_adjust(hspace=0.05)
+
+    pyplot.show()
