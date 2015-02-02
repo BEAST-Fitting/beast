87b8c877c8596efff2af0c6e9812bfa56cd966cf
Yumi Choi <ymchoi@uw.edu>
2015-02-02 03:21:45 -0800

    Updates
    Former-commit-id: 803f5f165c044217891b5883438a7f9b1ee70bfa

--- projects/ngc4214/pipeline.py
+++ projects/ngc4214/pipeline.py
@@ -25,7 +25,7 @@ import os
 
 import datamodel
 import noisemodel
-from models import t_isochrones, t_spectra, t_seds
+from models import t_isochrones, t_spectra, t_seds, t_priors
 from fit import t_fit, t_summary_table
 from noisemodel import t_gen_noise_model
 
@@ -183,6 +183,7 @@ def make_models(*args, **kwargs):
     Equivalent to using individual tasks as follow:
     project, noisefile, grid = project | t_isochrones(**iso_kwargs)
                                        | t_spectra(**spec_kwargs)
+                                       | t_priors()
                                        | t_seds(filters, **seds_kwargs)
                                        | t_gen_noise_model(astfile, **noise_kwargs)
 
@@ -200,7 +201,8 @@ def make_models(*args, **kwargs):
     iso_kwargs = dict(logtmin=datamodel.logt[0],
                       logtmax=datamodel.logt[1],
                       dlogt=datamodel.logt[2],
-                      z=datamodel.z)
+                      z=datamodel.z,
+                      trackVersion=datamodel.trackVersion)
 
     dmod = val_in_unit('distance Modulus', datamodel.distanceModulus, 'mag').magnitude
     distance = 10 ** ( (dmod / 5.) + 1 ) * unit['pc']
@@ -216,17 +218,51 @@ def make_models(*args, **kwargs):
         seds_kwargs['add_spectral_properties_kwargs'] = datamodel.add_spectral_properties_kwargs
         spec_kwargs['add_spectral_properties_kwargs'] = datamodel.add_spectral_properties_kwargs
 
-    noise_kwargs = dict(covariance=datamodel.calibration_covariance)
+    #noise_kwargs = dict(covariance=datamodel.absflux_a_matrix)
 
     # make models if not there yet
     tasks_models = (t_project_dir,
                     t_isochrones(**iso_kwargs),
                     t_spectra(**spec_kwargs),
-                    t_seds(datamodel.filters, **seds_kwargs),
-                    t_gen_noise_model(datamodel.astfile, **noise_kwargs))
+                    t_seds(datamodel.filters, **seds_kwargs))#,
+                    #t_gen_noise_model(datamodel.astfile, **noise_kwargs))
 
     models = Pipeline('make_models', tasks_models)
-    job, (p, n, g) = models(datamodel.project)
+    job, (p, g) = models(datamodel.project)
+
+    return job, (p, g)
+
+def compute_noise_and_trim_grid(*args, **kwargs):
+    """ compute the noise model for a specific catalog and
+        trim bright/faint models to speed up the calcuations
+
+    1. compute the noise model given the ASTs and absflux A matrix
+    2. trim the model sed grid of models that are so bright
+       or faint that will always get a zero fit probability (speed optimization)
+
+    Equivalent to using individual tasks as follow:
+    project, noisefile, grid = project | t_gen_noise_model(datamodel.astfile, **noise_kwargs)
+                                       | t_trim_model_sed(????)
+
+    returns
+    -------
+    job: int
+        job id
+
+    (p, n, g): project, noisemodel, ModelGrid
+        project identification
+        noise model file
+        Modelgrid instance constaining the collection of SEDs
+    """
+    # calling sequences
+
+    noise_kwargs = dict(covariance=datamodel.absflux_a_matrix)
+
+    # make models if not there yet
+    tasks_noise_and_trim = (t_gen_noise_model(datamodel.astfile, **noise_kwargs))
+
+    noise_and_trim = Pipeline('compute_noise_and_trim_grid', tasks_noise_and_trim)
+    job, (p, n, g) = noise_and_trim(datamodel.project)
 
     return job, (p, n, g)
 
@@ -270,7 +306,7 @@ def run_fit(project, g, noise=None, obsfile=None):
                          distanceModulus=datamodel.distanceModulus,
                          filters=datamodel.filters)
 
-    fit_kwargs = dict( threshold=-10 )
+    fit_kwargs = dict( threshold=-5.0 )
 
     stat_kwargs = dict( keys=None, method=None )
 
@@ -344,7 +380,7 @@ def run_chunk_fit(project, g, chunk, noise=None, obsfile=None):
     outname = project[:]
     l_file = '{0:s}/{0:s}.part{1:d}'.format(outname, chunk)
 
-    fit_kwargs = dict( threshold=-10, outname=l_file )
+    fit_kwargs = dict( threshold=-5.0, outname=l_file )
 
     stat_kwargs = dict( keys=None, method=None, outname=l_file)
 
