75726cddd34e1b1bfe50c2436364dfa7d62f30ea
Karl Gordon <kgordon@stsci.edu>
2017-02-21 13:20:54 -0500

    Minor update

--- beast/examples/phat_small/run_beast.py
+++ beast/examples/phat_small/run_beast.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """
 Script to run the BEAST on the PHAT-like data.
-Assumes that the datamodel.py file exists in the directory where this 
-   script is run.
+Assumes that the datamodel.py file exists in the same directory as this script.
+  And it must be called datamodel.py 
+     used in make_model.py code in physicsmodel, more recoding needed to remove
+     this dependency
 """
 
 # system imports
