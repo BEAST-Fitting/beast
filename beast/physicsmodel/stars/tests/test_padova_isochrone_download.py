dbb1086380128486db82e8252f83e57dced1e901
Karl Gordon <kgordon@stsci.edu>
2017-11-21 14:45:00 -0500

    First regression test using cached remote data

---
+++ beast/physicsmodel/stars/tests/test_padova_isochrone_download.py
@@ -0,0 +1,42 @@
+import os.path
+import filecmp
+
+import numpy as np
+
+from astropy.utils.data import download_file
+from astropy.tests.helper import remote_data
+from astropy.table import Table
+
+from beast.physicsmodel.stars import isochrone
+
+@remote_data
+def test_padova_isochrone_download():
+    # download the cached version
+    filename = download_file('http://www.stsci.edu/~kgordon/beast/beast_example_phat_iso_new.csv', cache=True)
+    table_cache = Table.read(filename, format='ascii.csv', comment='#',
+                             delimiter=',')
+    #table_cache = Table.read(filename)
+    
+    # initialize isochrone
+    oiso = isochrone.PadovaWeb()
+
+    # download from the padova website
+    t = oiso._get_t_isochrones(6.0, 10.13, 1.0, [0.03, 0.019, 0.008, 0.004])
+    t.header['NAME'] = 'Test of Cached Isochrones'
+
+    # save
+    savename = '/tmp/padova_iso.csv'
+    t.write(savename)
+
+    # get the new table
+    table_new = Table.read(savename, format='ascii.csv', comment='#',
+                             delimiter=',')
+    
+    # compare
+    assert len(table_new) == len(table_cache)
+
+    for tcolname in table_new.colnames:
+        np.testing.assert_equal(table_new[tcolname], table_cache[tcolname],
+                                '%s columns not equal'%tcolname)
+
+                       
