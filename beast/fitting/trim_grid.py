b73db307c243ad5bfded5666af129831583b5795
Karl Gordon <kgordon@stsci.edu>
2018-09-13 11:26:49 -0400

    Cleanup trim_grid and PEP8

--- beast/fitting/trim_grid.py
+++ beast/fitting/trim_grid.py
@@ -5,7 +5,7 @@ Trim the grid of models
 For a given set of observations, there will be models that are so
 bright or faint that they will always have ~0 probability of fitting
 the data.  This program trims those models out of the SED grid
-so that time is not spent calculating model points that are always 
+so that time is not spent calculating model points that are always
 zero probability.
 """
 from __future__ import (absolute_import, division, print_function,
@@ -14,11 +14,11 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 import tables
 
-from ..physicsmodel.grid import FileSEDGrid
 from ..physicsmodel.grid import SpectralGrid
 from ..external.eztables import Table
 
 __all__ = ['trim_models']
+
 
 def trim_models(sedgrid, sedgrid_noisemodel, obsdata, sed_outname,
                 noisemodel_outname,
@@ -28,43 +28,41 @@ def trim_models(sedgrid, sedgrid_noisemodel, obsdata, sed_outname,
 
     Keywords
     ---------
-    sedgrid: str or grid.SEDgrid instance
-    	model grid
+    sedgrid: grid.SEDgrid instance
+        model grid
 
     sedgrid_noisemodel: beast noisemodel instance
-    	noise model data
+        noise model data
 
     obsdata: Observation object instance
-    	observation catalog
+        observation catalog
 
     sed_outname: str
-	name for output sed file
+        name for output sed file
 
     noisemodel_outname: str
-    	name for output noisemodel file
+        name for output noisemodel file
 
     sigma_fac: float
-    	factor for trimming the upper and lower range of grid so that
-	the model range cuts off sigma_fac above and below the brightest
-	and faintest models, respectively (default: 3.)
+        factor for trimming the upper and lower range of grid so that
+        the model range cuts off sigma_fac above and below the brightest
+        and faintest models, respectively (default: 3.)
 
     n_detected: int
-    	minimum number of bands where ASTs yielded a detection for
-	a given model, if fewer detections than n_detected this model
-	gets eliminated (default: 4)
-    		
+        minimum number of bands where ASTs yielded a detection for
+        a given model, if fewer detections than n_detected this model
+        gets eliminated (default: 4)
+
     inFlux: boolean
-	if true data are in fluxes (default: True) 
+        if true data are in fluxes (default: True)
 
     trunchen: boolean
-    	if true use the trunchen noise model (default: False)
-
+        if true use the trunchen noise model (default: False)
 
     returns
     --------
     N/A
     """
-    
     # Store the brigtest and faintest fluxes in each band (for data and asts)
     n_filters = len(obsdata.filters)
     min_data = np.zeros(n_filters)
@@ -74,18 +72,18 @@ def trim_models(sedgrid, sedgrid_noisemodel, obsdata, sed_outname,
     for k, filtername in enumerate(obsdata.filters):
         sfiltname = obsdata.data.resolve_alias(filtername)
         if inFlux:
-            min_data[k] = np.amin(obsdata.data[sfiltname]*
-                                  obsdata.vega_flux[k])
-            max_data[k] = np.amax(obsdata.data[sfiltname]*
-                                  obsdata.vega_flux[k])
+            min_data[k] = np.amin(obsdata.data[sfiltname]
+                                  * obsdata.vega_flux[k])
+            max_data[k] = np.amax(obsdata.data[sfiltname]
+                                  * obsdata.vega_flux[k])
         else:
-            min_data[k] = np.amin(10 **(-0.4*obsdata.data[sfiltname])
-                                  *obsdata.vega_flux[k])
-            max_data[k] = np.amax(10 **(-0.4*obsdata.data[sfiltname])
-                                  *obsdata.vega_flux[k])
+            min_data[k] = np.amin(10**(-0.4*obsdata.data[sfiltname])
+                                  * obsdata.vega_flux[k])
+            max_data[k] = np.amax(10**(-0.4*obsdata.data[sfiltname])
+                                  * obsdata.vega_flux[k])
 
-        min_models[k] = np.amin(sedgrid.seds[:,k])
-        max_models[k] = np.amax(sedgrid.seds[:,k])
+        min_models[k] = np.amin(sedgrid.seds[:, k])
+        max_models[k] = np.amax(sedgrid.seds[:, k])
 
     # first remove all models that have any band with fluxes below the
     #    faintest ASTs run
@@ -98,12 +96,12 @@ def trim_models(sedgrid, sedgrid_noisemodel, obsdata, sed_outname,
     #   probability should always be zero
     model_unc = sedgrid_noisemodel.root.error[:]
     above_ast = (model_unc > 0)
-    sum_above_ast = np.sum(above_ast,axis=1)
+    sum_above_ast = np.sum(above_ast, axis=1)
     indxs, = np.where(sum_above_ast >= n_detected)
 
     # cache the noisemodel values
     model_bias = sedgrid_noisemodel.root.bias[:]
-    model_unc = np.fabs(sedgrid_noisemodel.root.error[:])  
+    model_unc = np.fabs(sedgrid_noisemodel.root.error[:])
     model_compl = sedgrid_noisemodel.root.completeness[:]
     if trunchen:
         model_q_norm = sedgrid_noisemodel.root.q_norm[:]
@@ -120,11 +118,11 @@ def trim_models(sedgrid, sedgrid_noisemodel, obsdata, sed_outname,
     for k in range(n_filters):
         print('working on filter # = ', k)
 
-        # Get upper and lower values for the models given the noise model 
+        # Get upper and lower values for the models given the noise model
         #  sigma_fac defaults to 3.
-        model_val = sedgrid.seds[indxs,k] + model_bias[indxs,k]
-        model_down = model_val - sigma_fac*model_unc[indxs,k]
-        model_up = model_val + sigma_fac*model_unc[indxs,k]
+        model_val = sedgrid.seds[indxs, k] + model_bias[indxs, k]
+        model_down = model_val - sigma_fac*model_unc[indxs, k]
+        model_up = model_val + sigma_fac*model_unc[indxs, k]
 
         nindxs, = np.where((model_up >= min_data[k]) &
                            (model_down <= max_data[k]))
@@ -135,38 +133,36 @@ def trim_models(sedgrid, sedgrid_noisemodel, obsdata, sed_outname,
         print('no models that are within the data range')
         exit()
 
-    print('number of original models = ', len(sedgrid.seds[:,0]))
+    print('number of original models = ', len(sedgrid.seds[:, 0]))
     print('number of ast trimmed models = ', n_ast_indxs)
     print(' number of trimmed models = ', len(indxs))
 
-    #Save the grid
+    # Save the grid
     print('Writing trimmed sedgrid to disk into {0:s}'.format(sed_outname))
     cols = {}
     for key in list(sedgrid.grid.keys()):
         cols[key] = sedgrid.grid[key][indxs]
 
     # New column to save the index of the model in the full grid
-    cols['fullgrid_idx'] = indxs.astype(int) 
+    cols['fullgrid_idx'] = indxs.astype(int)
     g = SpectralGrid(sedgrid.lamb, seds=sedgrid.seds[indxs],
                      grid=Table(cols), backend='memory')
     filternames = obsdata.filters
     g.grid.header['filters'] = ' '.join(filternames)
 
-    g.writeHDF(sed_outname) # trimmed grid name
+    # trimmed grid name
+    g.writeHDF(sed_outname)
 
     # save the trimmed noise model
-    print('Writing trimmed noisemodel to disk into {0:s}'.\
+    print('Writing trimmed noisemodel to disk into {0:s}'.
           format(noisemodel_outname))
     with tables.open_file(noisemodel_outname, 'w') as outfile:
-        outfile.create_array(outfile.root,'bias', model_bias[indxs])
-        outfile.create_array(outfile.root,'error', model_unc[indxs])
-        outfile.create_array(outfile.root,'completeness', model_compl[indxs])
+        outfile.create_array(outfile.root, 'bias', model_bias[indxs])
+        outfile.create_array(outfile.root, 'error', model_unc[indxs])
+        outfile.create_array(outfile.root, 'completeness', model_compl[indxs])
         if trunchen:
-            outfile.create_array(outfile.root,'q_norm', model_q_norm[indxs])
-            outfile.create_array(outfile.root,'icov_diag',
-                                model_icov_diag[indxs])
-            outfile.create_array(outfile.root,'icov_offdiag',
-                                model_icov_offdiag[indxs])
-            
-
-
+            outfile.create_array(outfile.root, 'q_norm', model_q_norm[indxs])
+            outfile.create_array(outfile.root, 'icov_diag',
+                                 model_icov_diag[indxs])
+            outfile.create_array(outfile.root, 'icov_offdiag',
+                                 model_icov_offdiag[indxs])
