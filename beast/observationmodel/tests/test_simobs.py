09d632e65d0c316997fafd94a6a4d08dd9312764
Karl Gordon <kgordon@stsci.edu>
2018-09-14 17:39:01 -0400

    All tests updated to use common helper functions (and PEP8ed)

--- beast/observationmodel/tests/test_simobs.py
+++ beast/observationmodel/tests/test_simobs.py
@@ -1,40 +1,22 @@
-import os.path
-
-import numpy as np
-
-from astropy.utils.data import download_file
 from astropy.tests.helper import remote_data
 from astropy.table import Table
 
 from ..noisemodel import generic_noisemodel as noisemodel
 from ...physicsmodel.grid import FileSEDGrid
 from beast.observationmodel.observations import gen_SimObs_from_sedgrid
-
-
-def _download_rename(filename):
-    """
-    Download a file and rename it to have the right extension
-
-    Otherwise, downloaded file will not have an extension at all
-    """
-    url_loc = 'http://www.stsci.edu/~kgordon/beast/'
-    fname_dld = download_file('%s%s' % (url_loc, filename))
-    extension = filename.split('.')[-1]
-    fname = '%s.%s' % (fname_dld, extension)
-    os.rename(fname_dld, fname)
-    return fname
+from beast.tests.helpers import (download_rename, compare_tables)
 
 
 @remote_data
 def test_trim_grid():
 
     # download the needed files
-    vega_fname = _download_rename('vega.hd5')
-    seds_fname = _download_rename('beast_example_phat_seds.grid.hd5')
-    noise_fname = _download_rename('beast_example_phat_noisemodel.hd5')
+    vega_fname = download_rename('vega.hd5')
+    seds_fname = download_rename('beast_example_phat_seds.grid.hd5')
+    noise_fname = download_rename('beast_example_phat_noisemodel.hd5')
 
     # download cached version of noisemodel on the sed grid
-    simobs_fname_cache = _download_rename('beast_example_phat_simobs.fits')
+    simobs_fname_cache = download_rename('beast_example_phat_simobs.fits')
 
     ################
 
@@ -53,8 +35,4 @@ def test_trim_grid():
     # check that the simobs files are exactly the same
     table_cache = Table.read(simobs_fname_cache)
 
-    assert len(table_new) == len(table_cache)
-
-    for tcolname in table_new.colnames:
-        np.testing.assert_equal(table_new[tcolname], table_cache[tcolname],
-                                '%s columns not equal' % tcolname)
+    compare_tables(table_cache, table_new)
--- beast/observationmodel/tests/test_toothpick_noisemodel.py
+++ beast/observationmodel/tests/test_toothpick_noisemodel.py
@@ -1,45 +1,23 @@
-import os.path
-
-import numpy as np
-import h5py
-
-from astropy.utils.data import download_file
 from astropy.tests.helper import remote_data
 
 from ..noisemodel import generic_noisemodel as noisemodel
 from ..noisemodel.absflux_covmat import hst_frac_matrix
 from ...physicsmodel.grid import FileSEDGrid
-
-
-def _download_rename(filename):
-    """
-    Download a file and rename it to have the right extension
-
-    Otherwise, downloaded file will not have an extension at all
-    """
-    url_loc = 'http://www.stsci.edu/~kgordon/beast/'
-    fname_dld = download_file('%s%s' % (url_loc, filename))
-    extension = filename.split('.')[-1]
-    fname = '%s.%s' % (fname_dld, extension)
-    os.rename(fname_dld, fname)
-    return fname
+from beast.tests.helpers import (download_rename, compare_hdf5)
 
 
 @remote_data
-# @pytest.mark.skip(reason="temporarily disable")
 def test_toothpick_noisemodel():
 
     # download the needed files
-    asts_fname = _download_rename('fake_stars_b15_27_all.hd5')
-    filter_fname = _download_rename('filters.hd5')
-    vega_fname = _download_rename('vega.hd5')
-    hst_fname = _download_rename('hst_whitedwarf_frac_covar.fits')
-    seds_fname = _download_rename('beast_example_phat_seds.grid.hd5')
+    asts_fname = download_rename('fake_stars_b15_27_all.hd5')
+    filter_fname = download_rename('filters.hd5')
+    vega_fname = download_rename('vega.hd5')
+    hst_fname = download_rename('hst_whitedwarf_frac_covar.fits')
+    seds_fname = download_rename('beast_example_phat_seds.grid.hd5')
 
     # download cached version of noisemodel on the sed grid
-    filename = _download_rename('beast_example_phat_noisemodel.hd5')
-
-    hdf_cache = h5py.File(filename, 'r')
+    noise_fname_cache = download_rename('beast_example_phat_noisemodel.hd5')
 
     ################
     # get the modesedgrid on which to generate the noisemodel
@@ -52,8 +30,6 @@ def test_toothpick_noisemodel():
                                        hst_fname=hst_fname,
                                        filterLib=filter_fname)
 
-#    ast_colnames = np.array(['F275W', 'F336W', 'F475W',
-#                             'F814W', 'F110W', 'F160W'])
     # generate the AST noise model
     noise_fname = '/tmp/beast_example_phat_noisemodel.hd5'
     noisemodel.make_toothpick_noise_model(
@@ -63,27 +39,5 @@ def test_toothpick_noisemodel():
                             absflux_a_matrix=absflux_a_matrix,
                             vega_fname=vega_fname)
 
-    # open the hdf file with the specral grid
-    hdf_new = h5py.File(noise_fname, 'r')
-
-    # go through the file and check if it is exactly the same
-    for sname in hdf_cache.keys():
-        if isinstance(hdf_cache[sname], h5py.Dataset):
-            cvalue = hdf_cache[sname]
-            cvalue_new = hdf_new[sname]
-            if cvalue.dtype.fields is None:
-                np.testing.assert_allclose(cvalue.value, cvalue_new.value,
-                                           err_msg='testing %s' % (sname),
-                                           rtol=1e-6)
-            else:
-                for ckey in cvalue.dtype.fields.keys():
-                    err_msg = 'testing %s/%s' % (sname, ckey)
-                    np.testing.assert_allclose(cvalue.value[ckey],
-                                               cvalue_new.value[ckey],
-                                               err_msg=err_msg,
-                                               rtol=1e-5)
-
-
-if __name__ == '__main__':
-
-    test_toothpick_noisemodel()
+    # compare the new to the cached version
+    compare_hdf5(noise_fname_cache, noise_fname)
