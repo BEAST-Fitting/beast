e1cb25e3def78ffad7d9917b4e811831a7e78954
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2018-10-05 17:54:07 +0200

    Test for reduce_grid_info

--- beast/tools/tests/test_subgridding_tools.py
+++ beast/tools/tests/test_subgridding_tools.py
@@ -48,4 +48,22 @@ def test_split_grid():
 
 @remote_data
 def test_reduce_grid_info():
-    pass
+    seds_trim_fname = download_rename('beast_example_phat_seds_trim.grid.hd5')
+    sub_fnames = subgridding_tools.split_grid(seds_trim_fname, 3)
+
+    complete_g_info = subgridding_tools.subgrid_info(seds_trim_fname)
+    cap_unique = 50
+    sub_g_info = subgridding_tools.reduce_grid_info(
+        sub_fnames, nprocs=3, cap_unique=cap_unique)
+
+    for q in complete_g_info:
+        assert q in sub_g_info
+        assert complete_g_info[q]['min'] == sub_g_info[q]['min']
+        assert complete_g_info[q]['max'] == sub_g_info[q]['max']
+        num_unique = len(complete_g_info[q]['unique'])
+        if num_unique > cap_unique:
+            # Cpan still be larger if one of the sub results during the
+            # reduction is larger. This is as intended.
+            assert sub_g_info[q]['num_unique'] >= cap_unique
+        else:
+            assert sub_g_info[q]['num_unique'] == num_unique
