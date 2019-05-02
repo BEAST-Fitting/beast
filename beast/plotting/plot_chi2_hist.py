43bc573b7b19bf3b82c35204fc217f704546b07c
Lea Hagen <lhagen@stsci.edu>
2019-05-02 10:00:21 -0400

    another lgtm fix

--- beast/plotting/plot_chi2_hist.py
+++ beast/plotting/plot_chi2_hist.py
@@ -2,7 +2,6 @@ from __future__ import print_function, division
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
-import re
 
 
 def plot(beast_stats_file, n_bins=50):
