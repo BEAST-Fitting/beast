d1b279de1fcd4d1cbb77a96d88380d818034f10e
Lea Hagen <lhagen@stsci.edu>
2019-07-05 11:12:48 -0400

    new function with input file names (rather than constructing file names)

--- beast/tools/setup_batch_beast_trim.py
+++ beast/tools/setup_batch_beast_trim.py
@@ -143,7 +143,139 @@ def setup_batch_beast_trim(project,
     for i in range(n_subtrim_files):
         bt_f[i].close()
 
+def generic_batch_trim(model_grid_file,
+                           noise_model_files,
+                           data_files,
+                           ast_files,
+                           trimmed_file_bases,
+                           job_path=None,
+                           file_prefix='BEAST',
+                           num_subtrim=5,
+                           nice=None,
+                           prefix=None):
+    """
+    Sets up batch files for submission to the 'at' queue on
+    linux (or similar) systems
 
+    This is a more generic version of the above code, in which all
+    files are specified at the input, rather than constructed.
+
+    The parameters that are lists of strings need to be the same length.
+
+    Parameters
+    ----------
+    model_grid_file : string
+        full filename to the SED grid (note that having the same model grid
+        file is what makes the batch trimming efficient)
+
+    noise_model_files : list of strings
+        full filename to the noise models
+
+    data_files : list of strings
+        file with the observed data (FITS file)
+
+    ast_files : list of strings
+        file with ASTs
+
+    trimmed_file_bases : string
+        prefix (path+name) for the eventual trimmed model grid and noise model
+
+    job_path : string (default=None)
+        path in which to store info about the jobs
+    
+    file_prefix : string (default='BEAST')
+        prefix for the name of the file in which to store the trimming commands
+
+    num_subtrim : int (default = 5)
+        number of trim batch jobs
+
+    nice : int (default = None)
+        set this to an integer (-20 to 20) to prepend a "nice" level
+        to the trimming command
+
+    prefix : string (default=None)
+        Set this to a string (such as 'source activate astroconda') to prepend
+        to each batch file (use '\n's to make multiple lines)
+
+    """
+
+
+    # check that everything is the same length that needs to be
+    keyword_list = [noise_model_files, data_files, ast_files, trimmed_file_bases]
+    length_array = np.array( [len(key) for key in keyword_list ] )
+    if len(np.unique(length_array)) != 1:
+        print('input lists are not the same length!')
+        return
+
+
+    # number of photometry catalog files
+    n_cat_files = len(data_files)
+
+    # make sure n_subtrim_files isn't larger than the number of
+    # catalog sub-files
+    if num_subtrim > n_cat_files:
+        num_subtrim = n_cat_files
+
+    # max number of files per process
+    n_per_subtrim = int(n_cat_files/num_subtrim)
+    if n_cat_files % num_subtrim != 0:
+        n_per_subtrim += 1
+
+    print('# trim files per process = ', n_per_subtrim)
+
+    # setup the subdirectory for the batch and log files
+    if job_path is None:
+        job_path = os.path.dirname(trimmed_file_bases[0])+'/trim_batch_jobs/'
+    if not os.path.isdir(job_path):
+        os.mkdir(job_path)
+
+    log_path = job_path+'logs/'
+    if not os.path.isdir(log_path):
+        os.mkdir(log_path)
+
+
+    # prepend nice
+    nice_str = ''
+    if nice is not None:
+        nice_str = 'nice -n' + str(int(nice)) + ' '
+
+    joblist_file = job_path+file_prefix+'_batch_trim.joblist'
+    pf = open(joblist_file, 'w')
+
+    # write out anything at the beginning of the file
+    if prefix is not None:
+        pf.write(prefix+'\n')
+
+    
+    bt_f = []
+    for i in range(num_subtrim):
+        trimfile = job_path+file_prefix+'_' + str(i+1)
+        bt_f.append(open(trimfile, 'w'))
+        bt_f[-1].write(model_grid_file + '\n')
+        pf.write(nice_str + 'python -m beast_lh.tools.trim_many_via_obsdata '
+                 + trimfile + ' > '
+                 + log_path + file_prefix+'_trim_tr'+str(i+1)+'.log\n')
+    pf.close()
+
+
+    k = 0
+    n_cur = 0
+    for i in range(n_cat_files):
+        bt_f[k].write(noise_model_files[i])
+        bt_f[k].write(' ' + data_files[i])
+        bt_f[k].write(' ' + ast_files[i])
+        bt_f[k].write(' ' + trimmed_file_bases[i])
+        bt_f[k].write('\n')
+        n_cur += 1
+        if n_cur >= n_per_subtrim:
+            n_cur = 0
+            k += 1
+
+    for i in range(num_subtrim):
+        bt_f[i].close()
+
+
+        
 if __name__ == '__main__':
 
     # commandline parser
