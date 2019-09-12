be5672df0ec88f03ffa056f78cbe873e9783a4fb
Karl Gordon <kgordon@stsci.edu>
2019-09-12 12:46:39 -0400

    Removing unused code from base physicsmodel subdir

--- beast/physicsmodel/create_project_dir.py
+++ beast/physicsmodel/create_project_dir.py
@@ -1,8 +1,6 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
 import os
 
-__all__ = ['create_project_dir']
+__all__ = ["create_project_dir"]
 
 
 def create_project_dir(project):
@@ -28,8 +26,8 @@ def create_project_dir(project):
     if os.path.exists(outdir):
         if not os.path.isdir(outdir):
             raise Exception(
-                'Output directory %s already exists but is not a directory'
-                % outdir)
+                "Output directory %s already exists but is not a directory" % outdir
+            )
     else:
         os.mkdir(outdir)
-    return '%s/%s' % (outdir, project)
+    return "%s/%s" % (outdir, project)
