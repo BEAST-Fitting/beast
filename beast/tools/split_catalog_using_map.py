3dc81e1cc7befb61829f91a5f5d2a0ac372ff3b1
Lea Hagen <lhagen@stsci.edu>
2019-10-03 10:58:58 -0400

    add option to split catalogs into subfiles of user-defined length

--- beast/tools/split_catalog_using_map.py
+++ beast/tools/split_catalog_using_map.py
@@ -31,27 +31,53 @@ def main():
     nbin_or_binwidth.add_argument("--bin_width", type=float, default=None,
                                       help="Width of the density bins for splitting catalog")
 
+    parser.add_argument("--n_per_file", type=int, default=6250,
+                            help="Number of sources per subfile")
+
     args = parser.parse_args()
 
-    split_main(args.catfile, args.astfile, args.mapfile, args.n, args.bin_width)
+    split_main(args.catfile, args.astfile, args.mapfile, args.n, args.bin_width, args.n_per_file)
 
 
-def split_main(catfile, astfile, mapfile, n_bin=None, bin_width=None):
+def split_main(catfile, astfile, mapfile, n_bin=None, bin_width=None, n_per_file=6250):
 
     # Create a binned density map, so both the observed and the ast
     # catalog can be split using a consistent grouping (= binning) of
     # the tiles
     bdm = BinnedDensityMap.create(mapfile, N_bins=n_bin, bin_width=bin_width)
 
-    split_catalog_using_map(catfile, bdm)
+    print("Splitting catalog")
+    split_catalog_using_map(catfile, bdm, n_per_file=n_per_file)
+    print("")
+    print("Splitting ASTs")
     split_catalog_using_map(
-        astfile, bdm, ra_colname="RA_J2000", dec_colname="DEC_J2000"
+        astfile, bdm, ra_colname="RA_J2000", dec_colname="DEC_J2000", n_per_file=None
     )
 
 
 def split_catalog_using_map(
-    catfile, binned_density_map, ra_colname="RA", dec_colname="DEC"
+    catfile, binned_density_map, ra_colname="RA", dec_colname="DEC", n_per_file=6250
 ):
+    """
+    Code to do the splitting of a catalog
+
+    Parameters
+    ----------
+    catfile : string
+        name of the photometry catalog file
+
+    binned_density_map : BinnedDensityMap object
+        the binned density map for the field
+
+    ra_colname, dec_colname : string
+        labels for the RA and DEC columns
+
+    n_per_file : int or None (default=6250)
+        If set, divide the split catalog into sub-catalogs with length
+        n_per_file.  Good for photometry, not useful for ASTs.
+
+
+    """
     cat = Table.read(catfile)
 
     ras = cat[ra_colname]
@@ -64,9 +90,29 @@ def split_catalog_using_map(
     binnrs = np.unique(bin_foreach_source)
 
     for b in binnrs:
+        # write out file for this bin
         sources_for_bin = np.where(bin_foreach_source == b)
+        print("bin {0}: {1} sources".format(b, len(sources_for_bin[0])) )
         subcat = cat[sources_for_bin]
-        subcat.write(catfile.replace(".fits", "_bin{}.fits".format(b)))
+        subcat.write(catfile.replace(".fits", "_bin{}.fits".format(b)), overwrite=True)
+
+        # write out sub-files, if chosen
+        if n_per_file is not None:
+
+            tot_subfiles = int(np.ceil( len(sources_for_bin[0]) / n_per_file))
+            print("dividing into " + str(tot_subfiles) + " subfiles for later fitting speed")
+
+            for i in range(tot_subfiles):
+                min_k = i * tot_subfiles
+                if i < tot_subfiles:
+                    max_k = (i + 1) * n_per_file
+                else:
+                    max_k = N
+
+                subcat[min_k:max_k].write(
+                    catfile.replace(".fits", "_bin{0}_sub{1}.fits".format(b, i)), overwrite=True
+                )
+
 
 
 if __name__ == "__main__":
