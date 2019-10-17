0f69906b30d05ae8690d4280e67c482a84d3e42b
John Wu <jwuphysics@gmail.com>
2019-10-17 14:14:27 -0400

    create simple test for creating extra filters

---
+++ beast/observationmodel/tests/test_extra_filters.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+from beast.observationmodel.extra_filters import make_integration_filter
+
+@pytest.mark.parametrize(
+    "lambda_start,lambda_finish,d_lambda",
+    [90., 913., 1.],
+)
+def test_extra_filters(lambda_start, lambda_finish, d_lambda):
+
+    # create example integration filter
+    f_int = make_integration_filter(
+        lambda_start, lambda_finish, d_lambda, "QION", observatory="Pseudo", instrument="Fake"
+    )
+
+    # test bandwidth and name
+    np.testing.assert_allclose(f_int.bandwidth, lambda_finish - lambda_start)
+    assert f_int.name == "Pseudo_Fake_QION"
+
+    # create example top hat filters
+    f_top = make_top_hat_filter(
+        lambda_start, lambda_finish, d_lambda, "TOP", observatory="Pseudo", instrument="Fake"
+    )
+
+    np.testing.assert_allclose(f_top.bandwidth, lambda_finish - lambda_start)
+    assert f_top.name == "Pseudo_Fake_TOP"
