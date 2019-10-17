67d11626453278665d34575df60bb492ba01046f
John Wu <jwuphysics@gmail.com>
2019-10-17 16:26:47 -0400

    remove implementation and blacked

--- beast/observationmodel/noisemodel/tests/test_splinter_noisemodel.py
+++ beast/observationmodel/noisemodel/tests/test_splinter_noisemodel.py
@@ -3,32 +3,23 @@ from beast.physicsmodel.grid import SpectralGrid
 
 import h5py
 import numpy as np
-import os
 import pytest
+
 
 @pytest.mark.parametrize("frac_unc", [0.01, 0.1, 0.3])
 def test_splinter_noisemodel(frac_unc):
 
     # make super simplified model SED grid
-    lamb = np.linspace(1000., 4000, 4)
+    lamb = np.linspace(1000.0, 4000, 4)
     seds = np.logspace(-4, -3, 4)[None, :] * np.array([1, 1.5])[:, None]
 
     modelsedgrid = SpectralGrid(
-        lamb=lamb,
-        seds=seds,
-        grid=[1],         # dummy input for now
-        backend="memory",
+        lamb=lamb, seds=seds, grid=[1], backend="memory"  # dummy input for now
     )
-
-    frac_unc = 0.1
 
     # make splinter noisemodel
     noise_fname = "/tmp/splinter_example_noisemodel_{:.2f}.grid.hd5".format(frac_unc)
-    make_splinter_noise_model(
-        noise_fname,
-        modelsedgrid,
-        frac_unc=frac_unc,
-    )
+    make_splinter_noise_model(noise_fname, modelsedgrid, frac_unc=frac_unc)
 
     # read entire noisemodel back in
     noisemodel = h5py.File(noise_fname)
