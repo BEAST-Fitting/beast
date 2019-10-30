4426eab7ce8a929be9979df09e0b0fc8a6c97fb9
cmurray-astro <clairemurray56@gmail.com>
2019-10-30 11:02:42 -0400

    modified test_plots.py, mainly resolving merge issues i think

--- beast/plotting/tests/test_plots.py
+++ beast/plotting/tests/test_plots.py
@@ -31,7 +31,7 @@ def _download_rename(filename):
 
 # @pytest.mark.skip(reason="awaiting resolution of pytest-mpl")
 @remote_data
-@pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare()
 def test_indiv_plot():
 
     # download cached version of fitting results
