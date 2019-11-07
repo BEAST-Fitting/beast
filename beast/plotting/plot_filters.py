34fe94db6ef4f584c7b29b8d35bbaff308beb66c
cmurray-astro <clairemurray56@gmail.com>
2019-11-07 15:32:54 -0500

    Fixed up filter plotting code, updated outdated arguments, made the filters look prettier

--- beast/plotting/plot_filters.py
+++ beast/plotting/plot_filters.py
@@ -11,8 +11,8 @@ from beast.plotting.beastplotlib import initialize_parser
 
 
 def plot_filters(
-    args, filter_names, save_name="beast_filters", xlim=[0.19, 2.0], ylim=[1e-8, 2e1]
-):
+    args, filter_names, save_name="beast_filters", xlim=[1.4e3, 2e4], ylim=[1e-4, 2]):
+
     """Plots transmission curves in log-log space.
 
     Parameters
@@ -27,11 +27,8 @@ def plot_filters(
         Values to set plot x-limits to
     ylim : length 2 list
         Values to set plot y-limits to
-
-    Returns
-    -------
-    Nothing.
     """
+
     fig, ax = plt.subplots(1, 1, figsize=(10, 6))
 
     # wavelength grid in angstroms for response functions
@@ -39,22 +36,25 @@ def plot_filters(
 
     # read in the filter response functions
     flist = phot.load_filters(filter_names, interp=True, lamb=waves)
-
-    color_indices = np.log10(np.array(sorted([f.lam_eff for f in flist])))
-    color_indices -= color_indices.min()
-    color_indices /= color_indices.max()
-    try:
-        cmap = mpl.cm.plasma
-    except AttributeError:  # for matplotlib version < 1.5
-        cmap = mpl.cm.rainbow
-    try:
-        ax.set_prop_cycle(color=[cmap(i) for i in color_indices])
-    except AttributeError:  # for matplotlib version < 1.5
-        ax.set_color_cycle(cmap(i) for i in color_indices)
+    # dict_keys(['name', 'wavelength', 'transmit', 'norm', 'lT', 'lpivot', 'cl'])
 
     for f in flist:
-        ax.plot(f.wavelength[f.nonzero], f.transmit[f.nonzero])
-        ax.text(f.lam_eff / 1e4, 0.1 * ylim[-1], f.name.split("_")[-1], ha="center")
+        print(f.norm)
+
+    color_indices = np.log10(np.array(np.sort([f.norm for f in flist])))
+    color_indices -= color_indices.min()
+    color_indices /= color_indices.max()
+
+    cmap = mpl.cm.plasma
+    # ax.set_prop_cycle(color=[cmap(i) for i in color_indices])
+    color=iter(cmap(np.linspace(0.2,0.8,len(filter_names))))
+
+    for f in flist:
+        c = next(color)
+        ax.plot(f.wavelength, f.transmit, color=c, lw=2)
+        ax.fill_between(f.wavelength, f.transmit, alpha=0.2, color=c)
+        ax.text(np.nanmean(f.wavelength[f.transmit>100.*ylim[0]]), 1.3*np.nanmax(f.transmit[f.transmit>ylim[0]]), f.name.split("_")[-1], ha="center", color=c)
+
 
     ax.set_xscale("log")
     ax.set_yscale("log")
@@ -63,10 +63,39 @@ def plot_filters(
     ax.set_xlabel(r"$\lambda$ [$\mu m$]")
     ax.set_ylabel(r"$B_i(\lambda)$")
 
-    ax.set_xticks([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0])
+    # ax.set_xticks([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0])
     ax.get_xaxis().set_major_formatter(mpl.ticker.ScalarFormatter())
 
     fig.tight_layout()
+    return fig
+
+
+if __name__ == "__main__":
+    parser = initialize_parser()
+    parser.add_argument(
+        "filter_names",
+        action="store",
+        default=['HST_WFC3_F225W', 'HST_WFC3_F275W', 'HST_WFC3_F336W',
+                            'HST_ACS_WFC_F475W', 'HST_ACS_WFC_F550M',
+                            'HST_ACS_WFC_F814W',
+                            'HST_WFC3_F110W', 'HST_WFC3_F160W'],
+        help="List of filters to plot",
+    )
+    parser.add_argument(
+        "--tex",
+        action="store",
+        default="False",
+        help="Use tex format for plot",
+    )
+    parser.add_argument(
+        "--savefig",
+        action="store",
+        default="True",
+        help="Save figure to file",
+    )
+    args = parser.parse_args()
+
+    fig = plot_filters(args, args.filter_names)
 
     if args.tex:
         plt.rc({"usetex": True})
@@ -74,16 +103,3 @@ def plot_filters(
         fig.savefig("{}.{}".format(save_name, args.savefig))
     else:
         plt.show()
-
-
-if __name__ == "__main__":
-    parser = initialize_parser()
-    args = parser.parse_args()
-
-
-    filter_names = ['HST_WFC3_F225W', 'HST_WFC3_F275W', 'HST_WFC3_F336W',
-                    'HST_ACS_WFC_F475W', 'HST_ACS_WFC_F550M',
-                    'HST_ACS_WFC_F814W',
-                    'HST_WFC3_F110W', 'HST_WFC3_F160W']
-
-    plot_filters(args, filter_names)
