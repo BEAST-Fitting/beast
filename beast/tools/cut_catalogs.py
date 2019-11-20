5ac296e58b23555e447322c6984d8fdc23ef4d38
Lea Hagen <lhagen@stsci.edu>
2019-11-20 09:43:40 -0500

    phot and AST catalogs cut together

--- beast/tools/cut_catalogs.py
+++ beast/tools/cut_catalogs.py
@@ -3,10 +3,14 @@ import argparse
 
 from astropy.table import Table
 
+from beast.observationmodel.ast.make_ast_xy_list import convexhull_path
+
 
 def cut_catalogs(
-    input_file,
-    output_file,
+    input_phot_file,
+    output_phot_file,
+    input_ast_file=None,
+    output_ast_file=None,
     partial_overlap=False,
     flagged=False,
     flag_filter=None,
@@ -14,21 +18,28 @@ def cut_catalogs(
     no_write=False,
 ):
     """
-    Remove sources from the input catalog that are
+    Remove sources from the input photometry catalog that are
     - in regions without full imaging coverage
     OR
     - flagged as bad in flag_filter
 
-    The input catalog can either be photometry or ASTs
+    Optionally also input an AST file to similarly cut.  If partial_overlap is
+    True, the overlap boundary found for the photometry catalog will be used to
+    trim the AST catalog.
 
     Parameters
     ----------
-    input_file : string
-        file name of the input catalog (photometry or AST), where data is in
-        extension 1 of the fits file
+    input_phot_file : string
+        file name of the photometry catalog
 
-    output_file : string
-        file name for the output catalog
+    output_phot_file : string
+        file name for the output photometry catalog
+
+    input_ast_file : string (default=None)
+        file name of the AST catalog
+
+    output_ast_file : string (default=None)
+        file name for the output AST catalog
 
     partial_overlap : boolean (default=False)
         if True, remove sources in regions without full imaging coverage.  This
@@ -45,11 +56,11 @@ def cut_catalogs(
         if flagged is True, set this to the filter(s) to use
 
     region_file : boolean (default=False)
-        if True, create a ds9 region file where good sources are green and
+        if True, create ds9 region file(s) where good sources are green and
         removed sources are magenta
 
     no_write : boolean (default=False)
-        if True, don't write out the catalog file.  Instead, just return it.
+        if True, don't write out the catalog file(s).  Instead, just return them.
 
     """
 
@@ -58,16 +69,98 @@ def cut_catalogs(
         print("must choose a criteria to cut catalogs")
         return
 
+    # run the cutting for the photometry file
+    phot_cat, phot_good_stars = make_cuts(
+        input_phot_file,
+        partial_overlap=partial_overlap,
+        flagged=flagged,
+        flag_filter=flag_filter,
+    )
+    print('removing {0} stars from {1}'.format(
+        int(len(phot_cat) - np.sum(phot_good_stars)), input_phot_file
+    ))
+    new_phot_cat = phot_cat[phot_good_stars == 1]
+
+    # if chosen, run the cutting for the AST file
+    if input_ast_file is not None:
+        ast_cat, temp_good_stars = make_cuts(
+            input_ast_file,
+            partial_overlap=partial_overlap,
+            flagged=flagged,
+            flag_filter=flag_filter,
+        )
+
+        # do convex hull
+        ra_col = [x for x in phot_cat.colnames if "RA" in x.upper()][0]
+        dec_col = [x for x in phot_cat.colnames if "DEC" in x.upper()][0]
+        phot_cat_boundary = convexhull_path(
+            new_phot_cat[ra_col], new_phot_cat[dec_col]
+        )
+        # check if points are inside
+        ra_col = [x for x in ast_cat.colnames if "RA" in x.upper()][0]
+        dec_col = [x for x in ast_cat.colnames if "DEC" in x.upper()][0]
+        within_bounds = phot_cat_boundary.contains_points(
+            np.array([ast_cat[ra_col], ast_cat[dec_col]]).T
+        )  # N,2 array of AST RA and Dec positions
+
+        # get final list of good stars
+        ast_good_stars = (temp_good_stars == 1) & (within_bounds == True)
+        print('removing {0} stars from {1}'.format(
+            int(len(ast_cat) - np.sum(ast_good_stars)), input_ast_file
+        ))
+        new_ast_cat = ast_cat[ast_good_stars]
+
+    # write out the sources as a ds9 region file
+    if region_file is True:
+        write_ds9(phot_cat, phot_good_stars, input_phot_file + ".reg")
+
+        if input_ast_file is not None:
+            write_ds9(ast_cat, ast_good_stars, input_ast_file + ".reg")
+
+
+    # either save it or return it
+    # - save it
+    if not no_write:
+        new_phot_cat.write(output_phot_file, format="fits", overwrite=True)
+        if input_ast_file is not None:
+            new_ast_cat.write(output_ast_file, format="fits", overwrite=True)
+    # - return it
+    else:
+        return new_phot_cat, phot_good_stars
+
+
+
+def make_cuts(cat_file, partial_overlap=False, flagged=False, flag_filter=None):
+    """
+    Wrapper to do overlap and flag cuts
+
+    Parameters
+    ----------
+    cat_file : string
+        name of the catalog file
+
+    partial_overlap : boolean (default=False)
+        see above
+
+    flagged : boolean (default=False)
+        see above
+
+    flag_filter : string (default=None)
+        see above
+
+    Returns
+    -------
+    cat : astropy table
+        the astropy table of the input catalog
+
+    good_stars : array
+        array in which 1=good star and 0=bad star
+    """
+
     # read in the catalog
-    cat = Table.read(input_file)
+    cat = Table.read(cat_file)
     filters = [c[0:-5] for c in cat.colnames if "RATE" in c and "RATERR" not in c]
     n_stars = len(cat)
-    if "RA" in cat.colnames:
-        ra_col = "RA"
-        dec_col = "DEC"
-    else:
-        ra_col = "RA_J2000"
-        dec_col = "DEC_J2000"
 
     # array to keep track of which ones are good
     # (1=good, 0=bad)
@@ -85,46 +178,47 @@ def cut_catalogs(
         for fl in np.atleast_1d(flag_filter):
             good_stars[(cat[fl + "_FLAG"] >= 99) & (cat[fl+"_RATE"] > 0)] = 0
 
-    # write out the sources as a ds9 region file
-    if region_file is True:
-        with open(input_file + ".reg", "w") as ds9_file:
+    # return results
+    return cat, good_stars
 
-            # header
-            ds9_file.write("# Region file format: DS9 version 4.1\n")
-            ds9_file.write(
-                'global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n'
-            )
-            ds9_file.write("fk5\n")
 
-            # stars
-            for i in range(n_stars):
-                if good_stars[i] == 0:
-                    ds9_file.write(
-                        "circle("
-                        + str(cat[ra_col][i])
-                        + ","
-                        + str(cat[dec_col][i])
-                        + ',0.1") # color=magenta\n'
-                    )
-                if good_stars[i] == 1:
-                    ds9_file.write(
-                        "circle("
-                        + str(cat[ra_col][i])
-                        + ","
-                        + str(cat[dec_col][i])
-                        + ',0.1")\n'
-                    )
+def write_ds9(cat, good_stars, ds9_file_name):
+    """
+    Make a ds9 region file
+    """
 
-    # make a new file with the bad stars removed
-    new_cat = cat[good_stars == 1]
+    ra_col = [x for x in cat.colnames if "RA" in x.upper()][0]
+    dec_col = [x for x in cat.colnames if "DEC" in x.upper()][0]
 
-    # either save it or return in
-    # - save it
-    if not no_write:
-        new_cat.write(output_file, format="fits", overwrite=True)
-    # - return in
-    else:
-        return new_cat, good_stars
+    with open(ds9_file_name, "w") as ds9_file:
+
+        # header
+        ds9_file.write("# Region file format: DS9 version 4.1\n")
+        ds9_file.write(
+            'global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n'
+        )
+        ds9_file.write("fk5\n")
+
+        # stars
+        for i in range(len(cat)):
+            if good_stars[i] == 0:
+                ds9_file.write(
+                    "circle("
+                    + str(cat[ra_col][i])
+                    + ","
+                    + str(cat[dec_col][i])
+                    + ',0.1") # color=magenta\n'
+                )
+            if good_stars[i] == 1:
+                ds9_file.write(
+                    "circle("
+                    + str(cat[ra_col][i])
+                    + ","
+                    + str(cat[dec_col][i])
+                    + ',0.1")\n'
+                )
+
+
 
 
 if __name__ == "__main__":  # pragma: no cover
