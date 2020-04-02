db89a7285218c494e756c19c9dfd2d04ab8de97a
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2020-04-02 15:05:47 -0400

    Update pdf1d.py

--- beast/fitting/pdf1d.py
+++ beast/fitting/pdf1d.py
@@ -86,7 +86,7 @@ class pdf1d:
             self.pdf_bin_indxs = pdf_bin_indxs
 
     def gen1d(self, gindxs, weights):
-        """Calculates the probabilities for each bin.
+        """Calculates the probabilities for each grid_weight bin.
 
         Parameters
         ----------
