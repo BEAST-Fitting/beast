f666b3cbb9c877200bcea0fe3b05b7966c87c893
mdecleir <mdecleir@stsci.edu>
2020-04-28 18:19:34 -0400

    Split the very long string

--- beast/tools/tests/test_write_sbatch_file.py
+++ beast/tools/tests/test_write_sbatch_file.py
@@ -7,6 +7,28 @@ def test_sbatch_file():
 
     file = open("create_LMC_mastergrid.script")
     content = file.read()
-    expected = "#!/bin/bash\n\n#SBATCH -J LMCgrid\n#SBATCH -o /pylon5/as5pi7p/lhagen/mastergrid_LMC/model_batch_jobs/logs/%A_%a.out\n#SBATCH -C EGRESS\n#SBATCH -p LM\n#SBATCH -t 35:00:00\n#SBATCH --mem 570GB\n#SBATCH --array=1-9\n\n# move to appropriate directory\ncd /pylon5/as5pi7p/lhagen\n\n# Load any necessary modules.\n# Loading modules in the script ensures a consistent environment.\nmodule load anaconda3\nsource activate bdev\n\n# Launch a job\n./mastergrid_LMC/model_batch_jobs/create_physicsmodel_\"${SLURM_ARRAY_TASK_ID}\".job\n\n"
+
+    expected = ("""#!/bin/bash
+
+#SBATCH -J LMCgrid
+#SBATCH -o /pylon5/as5pi7p/lhagen/mastergrid_LMC/model_batch_jobs/logs/%A_%a.out
+#SBATCH -C EGRESS
+#SBATCH -p LM
+#SBATCH -t 35:00:00
+#SBATCH --mem 570GB
+#SBATCH --array=1-9
+
+# move to appropriate directory
+cd /pylon5/as5pi7p/lhagen
+
+# Load any necessary modules.
+# Loading modules in the script ensures a consistent environment.
+module load anaconda3
+source activate bdev
+
+# Launch a job
+./mastergrid_LMC/model_batch_jobs/create_physicsmodel_\"${SLURM_ARRAY_TASK_ID}\".job
+
+""")
 
     assert content == expected, "The created sbatch file does not have the expected output."
