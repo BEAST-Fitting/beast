1e0e5fbdc12395f78886f7ef91054df8316ec92b
Lea Hagen <lhagen@stsci.edu>
2020-05-08 16:10:44 -0400

    testing complete for no subgrids

--- beast/tools/run/tests/test_create_physicsmodel.py
+++ beast/tools/run/tests/test_create_physicsmodel.py
@@ -1,7 +1,10 @@
-from beast.tools.run import create_physicsmodel
-from beast.tests.helpers import download_rename
+from beast.tests.helpers import download_rename, compare_hdf5, compare_tables
 from astropy.tests.helper import remote_data
 import os
+import importlib
+from astropy.table import Table
+
+import shutil
 
 @remote_data
 def test_create_physicsmodel_no_subgrid():
@@ -11,16 +14,34 @@ def test_create_physicsmodel_no_subgrid():
 
     # download files
     # - datamodel settings
-    datamodel_fname = download_rename("datamodel_no_subgrid.py")
+    #datamodel_fname = download_rename("datamodel_no_subgrid.py")
+    #os.rename(datamodel_fname, "datamodel.py")
+    shutil.copy2('/astro/dust_kg3/lhagen/stsci/beast-examples_lea-hagen/testing/datamodel_no_subgrid.py', './datamodel.py')
     # - input files (referenced in datamodel)
     filter_fname = download_rename("filters.hd5")
-    # - intermediate files (if they exist, create_physicsmodel won't
-    #   regenerate them, which is good -- that part doesn't need testing, and
-    #   there's a time limit for running things on Travis)
-    #priors_fname = download_rename("beast_example_phat_spec_w_priors.grid.hd5")
+    # - intermediate files
+    iso_fname_cache = download_rename("beast_example_phat_iso.csv")
+    priors_fname_cache = download_rename("beast_example_phat_spec_w_priors.grid.hd5")
     # - anticipated output files
-    seds_fname_1 = download_rename("beast_example_phat_seds.grid.hd5")
-    #seds_fname_2 = download_rename("beast_example_phat_seds.grid.hd5")
+    seds_fname_cache = download_rename("beast_example_phat_seds.grid.hd5")
+    # seds_fname_2 = download_rename("beast_example_phat_seds.grid.hd5")
 
-    # rename datamodel so it can be imported
-    os.rename(datamodel_fname, 'datamodel.py')
+    # run create_physicsmodel
+    # (the import is here because it'll break without datamodel being created first)
+    from beast.tools.run import create_physicsmodel
+    create_physicsmodel.create_physicsmodel(nsubs=1, nprocs=1)
+
+    # check that files match
+    # - isochrones
+    table_cache = Table.read(
+        iso_fname_cache, format="ascii.csv", comment="#", delimiter=",",
+    )
+    table_new = Table.read(
+        "./beast_no_subgrid/beast_no_subgrid_iso.csv",
+        format="ascii.csv", comment="#", delimiter=",",
+    )
+    compare_tables(table_cache, table_new)
+    # - spectra with priors
+    compare_hdf5(priors_fname_cache, "./beast_no_subgrid/beast_no_subgrid_spec_w_priors.grid.hd5")
+    # - SEDs grid
+    compare_hdf5(seds_fname_cache, "./beast_no_subgrid/beast_no_subgrid_seds.grid.hd5")
