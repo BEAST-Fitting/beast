a53fa4aa373e991e6ac6d29cc47db083d721127a
Lea Hagen <lhagen@stsci.edu>
2020-05-11 12:35:15 -0400

    change cached file names to remote version

--- beast/tools/run/tests/test_create_physicsmodel.py
+++ beast/tools/run/tests/test_create_physicsmodel.py
@@ -1,27 +1,20 @@
 from beast.tests.helpers import download_rename, compare_hdf5, compare_tables
 from astropy.tests.helper import remote_data
 import os
-import importlib
 from astropy.table import Table
-
-import shutil
 
 
 @remote_data
-def _test_create_physicsmodel_no_subgrid():
+def test_create_physicsmodel_no_subgrid():
     """
     Test create_physicsmodel, assuming no subgrids
     """
 
     # download files
     # - datamodel settings
-    # datamodel_fname = download_rename("datamodel_phat_small_no_subgrid.py")
-    # os.rename(datamodel_fname, "datamodel.py")
-    shutil.copy2(
-        "/astro/dust_kg3/lhagen/stsci/beast-examples_lea-hagen/testing/datamodel_no_subgrid.py",
-        "./datamodel.py",
-    )
-    # - input files (referenced in datamodel)
+    datamodel_fname = download_rename("datamodel_no_subgrid.py")
+    os.rename(datamodel_fname, "datamodel.py")
+    # - input files
     filter_fname = download_rename("filters.hd5")
     # - intermediate files
     iso_fname_cache = download_rename("beast_example_phat_iso.csv")
@@ -32,7 +25,6 @@ def _test_create_physicsmodel_no_subgrid():
     # run create_physicsmodel
     # (the import is here because it'll break without datamodel being created first)
     from beast.tools.run import create_physicsmodel
-
     create_physicsmodel.create_physicsmodel(nsubs=1, nprocs=1)
 
     # check that files match
@@ -61,39 +53,24 @@ def test_create_physicsmodel_with_subgrid():
     Test create_physicsmodel, assuming two subgrids
     """
 
-    prefix = "/astro/dust_kg3/lhagen/stsci/beast-examples_lea-hagen/testing/beast_example_phat_subgrids/"
-
     # download files
     # - datamodel settings
-    # datamodel_fname = download_rename("datamodel_phat_small_with_subgrid.py")
-    # os.rename(datamodel_fname, "datamodel.py")
-    shutil.copy2(
-        "/astro/dust_kg3/lhagen/stsci/beast-examples_lea-hagen/testing/datamodel_with_subgrid.py",
-        "./datamodel.py",
-    )
-    # - input files (referenced in datamodel)
+    datamodel_fname = download_rename("datamodel_with_subgrid.py")
+    os.rename(datamodel_fname, "datamodel.py")
+    # - input files
     filter_fname = download_rename("filters.hd5")
     # - intermediate files
     iso_fname_cache = download_rename("beast_example_phat_iso.csv")
     priors_fname_cache = download_rename("beast_example_phat_spec_w_priors.grid.hd5")
-    # priors_sub1_fname_cache = download_rename("beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5")
-    # priors_sub2_fname_cache = download_rename("beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5")
-    priors_sub0_fname_cache = (
-        prefix + "beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5"
-    )
-    priors_sub1_fname_cache = (
-        prefix + "beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5"
-    )
+    priors_sub1_fname_cache = download_rename("beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5")
+    priors_sub2_fname_cache = download_rename("beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5")
     # - anticipated output files
-    # seds_sub0_fname_cache = download_rename("beast_example_phat_subgrids_seds.gridsub0.hd5")
-    # seds_sub1_fname_cache = download_rename("beast_example_phat_subgrids_seds.gridsub1.hd5")
-    seds_sub0_fname_cache = prefix + "beast_example_phat_subgrids_seds.gridsub0.hd5"
-    seds_sub1_fname_cache = prefix + "beast_example_phat_subgrids_seds.gridsub1.hd5"
+    seds_sub0_fname_cache = download_rename("beast_example_phat_subgrids_seds.gridsub0.hd5")
+    seds_sub1_fname_cache = download_rename("beast_example_phat_subgrids_seds.gridsub1.hd5")
 
     # run create_physicsmodel
     # (the import is here because it'll break without datamodel being created first)
     from beast.tools.run import create_physicsmodel
-
     create_physicsmodel.create_physicsmodel(nsubs=2, nprocs=1)
 
     # check that files match
@@ -111,7 +88,6 @@ def test_create_physicsmodel_with_subgrid():
     compare_tables(table_cache, table_new)
 
     # - spectra with priors
-    print(os.listdir("beast_example_phat_subgrids/"))
     compare_hdf5(
         priors_fname_cache,
         "./beast_example_phat_subgrids/beast_example_phat_subgrids_spec_w_priors.grid.hd5",
