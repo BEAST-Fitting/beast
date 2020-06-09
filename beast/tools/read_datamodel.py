604a304ef3f34e4e9f94da9b617ff332b188d29a
Lea Hagen <lhagen@stsci.edu>
2020-06-09 16:31:31 -0400

    blacken code

--- beast/tools/read_datamodel.py
+++ beast/tools/read_datamodel.py
@@ -5,12 +5,9 @@
 	It must be called run_beast.py
 	Created by Maria Kapala on Feb 24th 2017
 """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
 
 from os.path import exists
 import numpy as np
-from sys import exit
 from astropy import units
 
 from beast.physicsmodel.stars import isochrone
@@ -18,7 +15,8 @@ from beast.physicsmodel.stars import stellib
 from beast.physicsmodel.dust import extinction
 from beast.observationmodel.noisemodel import absflux_covmat
 
-class datamodel():
+
+class datamodel:
     """
     All of the parameters from datamodel
 
@@ -28,7 +26,6 @@ class datamodel():
         name of the parameter file that was used for the input
 
     """
-
 
     def __init__(self, input_file):
         """
@@ -40,7 +37,7 @@ class datamodel():
         """
 
         self.datamodel_file = input_file
-        #import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
         if self.datamodel_file is not None:
             # read in the file
@@ -48,33 +45,36 @@ class datamodel():
             # verify parameters
             self.verify_params()
 
-
     def read_datamodel(self):
         """
         Read in the datamodel file and set parameters
         """
 
         # read everything in as strings
-        #import pdb; pdb.set_trace()
-        #input_data = Table.read(self.datamodel_file, format='ascii.no_header',
+        # import pdb; pdb.set_trace()
+        # input_data = Table.read(self.datamodel_file, format='ascii.no_header',
         #                            delimiter='=', comment='#')
-        #input_data = Table({'col1':['var','velocity','blah'],
+        # input_data = Table({'col1':['var','velocity','blah'],
         #                        'col2':['/path/to/somewhere','262.2 km/s','"some_file.txt"']})
-        with open(self.datamodel_file,'r') as f:
+        with open(self.datamodel_file, "r") as f:
             temp_data = f.readlines()
-        input_data = [line.strip() for line in temp_data if line.strip() != '' and line.strip()[0] != '#']
+        input_data = [
+            line.strip()
+            for line in temp_data
+            if line.strip() != "" and line.strip()[0] != "#"
+        ]
 
         # list of parameters that have units
         # (and therefor need special treatment)
-        params_with_units = ['distance_unit','velocity']
+        params_with_units = ["distance_unit", "velocity"]
 
         # parse it into a dictionary
         datamodel_params = {}
 
         for i in range(len(input_data)):
             # extract parameter and value (as strings)
-            param = input_data[i].split('=')[0].strip()
-            val = input_data[i].split('=')[1].strip()
+            param = input_data[i].split("=")[0].strip()
+            val = input_data[i].split("=")[1].strip()
             # if it's a unit, put into Quantity
             if param in params_with_units:
                 try:
@@ -87,12 +87,9 @@ class datamodel():
                 exec(input_data[i])
                 datamodel_params[param] = eval(param)
 
-
         # turn dictionary into attributes
         for key in datamodel_params:
             setattr(self, key, datamodel_params[key])
-
-
 
     def verify_params(self, noexit=False):
 
@@ -110,23 +107,40 @@ class datamodel():
 
         """
 
-        parameters_names = ['z', 'obsfile', 'astfile', 'logt', 'avs', 'rvs',
-                            'fAs']
+        parameters_names = ["z", "obsfile", "astfile", "logt", "avs", "rvs", "fAs"]
         parameters = [getattr(self, p) for p in parameters_names]
-        param_format = ['list_float', 'str_file', 'str_file',
-                        'list_float_grid', 'list_float_grid',
-                        'list_float_grid', 'list_float_grid']
-        parameters_limits = [ [0., 0.1], None, None, [-np.inf, 10.15],
-                            [0., np.inf], [1., 7.], [0., 1.]]
+        param_format = [
+            "list_float",
+            "str_file",
+            "str_file",
+            "list_float_grid",
+            "list_float_grid",
+            "list_float_grid",
+            "list_float_grid",
+        ]
+        parameters_limits = [
+            [0.0, 0.1],
+            None,
+            None,
+            [-np.inf, 10.15],
+            [0.0, np.inf],
+            [1.0, 7.0],
+            [0.0, 1.0],
+        ]
 
         for i, param_ in enumerate(parameters):
-           datamodel._verify_one_input_format(param_, parameters_names[i],
-                                     param_format[i], parameters_limits[i],
-                                     noexit=noexit)
+            datamodel._verify_one_input_format(
+                param_,
+                parameters_names[i],
+                param_format[i],
+                parameters_limits[i],
+                noexit=noexit,
+            )
 
     @staticmethod
-    def _verify_one_input_format(param, param_name, param_format, param_lim,
-                                        noexit=False):
+    def _verify_one_input_format(
+        param, param_name, param_format, param_lim, noexit=False
+    ):
         """
         Test for an input parameter correctness of format and limits.
 
@@ -150,44 +164,59 @@ class datamodel():
            Default = False (exit on error)
         """
 
-        if 'list' in param_format:
+        if "list" in param_format:
             if type(param) is not list:
                 if param is None:
-                    print('Warning: '+ param_name + ' is not defined.')
+                    print("Warning: " + param_name + " is not defined.")
                 else:
-                    print((param_name + ' is not in the right format - a list.'))
-                    if not noexit: exit()
-            elif 'float' in param_format:
+                    print((param_name + " is not in the right format - a list."))
+                    if not noexit:
+                        exit()
+            elif "float" in param_format:
                 is_list_of_floats = all(type(item) is float for item in param)
                 if not is_list_of_floats:
-                    print((param_name +
-                      ' is not in the right format - list of floats.'))
-                    if not noexit: exit()
-                elif 'grid' in param_format:
+                    print(
+                        (param_name + " is not in the right format - list of floats.")
+                    )
+                    if not noexit:
+                        exit()
+                elif "grid" in param_format:
                     # when param is aranged from given [min, max, step],
                     # instead of a specific list of values
                     datamodel._check_grid(param, param_name, param_lim, noexit=noexit)
                 else:
                     datamodel._verify_range(param, param_name, param_lim, noexit=noexit)
 
-            if 'str' in param_format:
+            if "str" in param_format:
                 if type(param) is not str:
-                    print((param_name + ' is not in the right format - a string.'))
-                    if not noexit: exit()
-                elif 'file' in param_format:
+                    print((param_name + " is not in the right format - a string."))
+                    if not noexit:
+                        exit()
+                elif "file" in param_format:
                     if not exists(param):
-                        print((param_name +
-                              ' does not exist. Please provide the file path.'))
-                        if not noexit: exit()
+                        print(
+                            (
+                                param_name
+                                + " does not exist. Please provide the file path."
+                            )
+                        )
+                        if not noexit:
+                            exit()
 
-            if 'version' in param_format:
+            if "version" in param_format:
                 if type(param) is not float:
-                    print((param_name + ' is not in the right format - a float'))
-                    if not noexit: exit()
+                    print((param_name + " is not in the right format - a float"))
+                    if not noexit:
+                        exit()
                 elif param not in param_lim:
-                    print((param_name +
-                        ' is an invalid number, leading to version of the isochrone.'))
-                    if not noexit: exit()
+                    print(
+                        (
+                            param_name
+                            + " is an invalid number, leading to version of the isochrone."
+                        )
+                    )
+                    if not noexit:
+                        exit()
 
     @staticmethod
     def _check_grid(param, param_name, param_lim, noexit=False):
@@ -196,32 +225,38 @@ class datamodel():
         param_min, param_max, param_step = param
 
         if param_min < param_lim[0]:
-            print((param_name + ' min value not physical.'))
-            if not noexit: exit()
+            print((param_name + " min value not physical."))
+            if not noexit:
+                exit()
 
         if param_max > param_lim[1]:
-            print((param_name + ' max value not physical.'))
-            if not noexit: exit()
+            print((param_name + " max value not physical."))
+            if not noexit:
+                exit()
 
         if param_min > param_max:
-            print((param_name + ' min value greater than max'))
-            if not noexit: exit()
+            print((param_name + " min value greater than max"))
+            if not noexit:
+                exit()
 
-        if (param_max-param_min) < param_step:
-            if param_max-param_min == 0.:
-                print('Note: '+param_name+' grid is single-valued.')
+        if (param_max - param_min) < param_step:
+            if param_max - param_min == 0.0:
+                print("Note: " + param_name + " grid is single-valued.")
             else:
-                print((param_name + ' step value greater than (max-min)'))
-                if not noexit: exit()
+                print((param_name + " step value greater than (max-min)"))
+                if not noexit:
+                    exit()
 
     @staticmethod
     def _verify_range(param, param_name, param_lim, noexit=False):
         # check if input param limits make sense
 
         if any(p < param_lim[0] for p in param):
-            print((param_name + ' min value not physical.'))
-            if not noexit: exit()
+            print((param_name + " min value not physical."))
+            if not noexit:
+                exit()
 
         if any(p > param_lim[1] for p in param):
-            print((param_name + ' max value not physical.'))
-            if not noexit: exit()
+            print((param_name + " max value not physical."))
+            if not noexit:
+                exit()
