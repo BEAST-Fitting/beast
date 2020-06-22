57b3b9a4a8bfc429a377ea3de3f437049abaa3f5
Lea Hagen <lhagen@stsci.edu>
2020-06-22 15:48:16 -0400

    update to use beast_settings, and blacken

--- beast/tools/run/tests/test_create_physicsmodel.py
+++ beast/tools/run/tests/test_create_physicsmodel.py
@@ -1,4 +1,6 @@
 from beast.tests.helpers import download_rename, compare_hdf5, compare_tables
+from beast.tools.tests import beast_settings_mockup
+from beast.tools.run import create_physicsmodel
 from astropy.tests.helper import remote_data
 import os
 from astropy.table import Table
@@ -10,10 +12,10 @@ def test_create_physicsmodel_no_subgrid():
     Test create_physicsmodel, assuming no subgrids
     """
 
+    # get beast settings mockup
+    settings = beast_settings_mockup.beast_settings_mockup()
+
     # download files
-    # - datamodel settings
-    datamodel_fname = download_rename("datamodel_no_subgrid.py")
-    os.rename(datamodel_fname, "datamodel.py")
     # - input files
     download_rename("filters.hd5")
     # - intermediate files
@@ -23,9 +25,9 @@ def test_create_physicsmodel_no_subgrid():
     seds_fname_cache = download_rename("beast_example_phat_seds.grid.hd5")
 
     # run create_physicsmodel
-    # (the import is here because it'll break without datamodel being created first)
-    from beast.tools.run import create_physicsmodel
-    create_physicsmodel.create_physicsmodel(nsubs=1, nprocs=1)
+    create_physicsmodel.create_physicsmodel(
+        settings, nsubs=settings.n_subgrid, nprocs=1
+    )
 
     # check that files match
     # - isochrones
@@ -53,25 +55,35 @@ def test_create_physicsmodel_with_subgrid():
     Test create_physicsmodel, assuming two subgrids
     """
 
+    # get beast settings mockup
+    settings = beast_settings_mockup.beast_settings_mockup()
+    # edit to have 2 subgrids
+    settings.n_subgrid = 2
+
     # download files
-    # - datamodel settings
-    datamodel_fname = download_rename("datamodel_with_subgrid.py")
-    os.rename(datamodel_fname, "datamodel.py")
     # - input files
     download_rename("filters.hd5")
     # - intermediate files
     iso_fname_cache = download_rename("beast_example_phat_iso.csv")
     priors_fname_cache = download_rename("beast_example_phat_spec_w_priors.grid.hd5")
-    priors_sub0_fname_cache = download_rename("beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5")
-    priors_sub1_fname_cache = download_rename("beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5")
+    priors_sub0_fname_cache = download_rename(
+        "beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5"
+    )
+    priors_sub1_fname_cache = download_rename(
+        "beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5"
+    )
     # - anticipated output files
-    seds_sub0_fname_cache = download_rename("beast_example_phat_subgrids_seds.gridsub0.hd5")
-    seds_sub1_fname_cache = download_rename("beast_example_phat_subgrids_seds.gridsub1.hd5")
+    seds_sub0_fname_cache = download_rename(
+        "beast_example_phat_subgrids_seds.gridsub0.hd5"
+    )
+    seds_sub1_fname_cache = download_rename(
+        "beast_example_phat_subgrids_seds.gridsub1.hd5"
+    )
 
     # run create_physicsmodel
-    # (the import is here because it'll break without datamodel being created first)
-    from beast.tools.run import create_physicsmodel
-    create_physicsmodel.create_physicsmodel(nsubs=2, nprocs=1)
+    create_physicsmodel.create_physicsmodel(
+        settings, nsubs=settings.n_subgrid, nprocs=1
+    )
 
     # check that files match
 
