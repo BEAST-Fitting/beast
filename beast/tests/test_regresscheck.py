414d348449164197a09bda0ced4bb4d41286617f
Lea Hagen <lhagen@stsci.edu>
2020-06-23 17:39:26 -0400

    add create_physicsmodel tests from #559

--- beast/tests/test_regresscheck.py
+++ beast/tests/test_regresscheck.py
@@ -42,6 +42,7 @@ from beast.tools.read_beast_data import (
     get_lnp_grid_vals,
 )
 from beast.tools.compare_spec_type import compare_spec_type
+from beast.tools.run import create_physicsmodel
 
 from beast.tests.helpers import (
     download_rename,
@@ -67,7 +68,19 @@ class TestRegressionSuite:
     iso_fname_cache = download_rename("beast_example_phat_iso.csv")
     spec_fname_cache = download_rename("beast_example_phat_spec_grid.hd5")
     priors_fname_cache = download_rename("beast_example_phat_spec_w_priors.grid.hd5")
+    priors_sub0_fname_cache = download_rename(
+        "beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5"
+    )
+    priors_sub1_fname_cache = download_rename(
+        "beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5"
+    )
     seds_fname_cache = download_rename("beast_example_phat_seds.grid.hd5")
+    seds_sub0_fname_cache = download_rename(
+        "beast_example_phat_subgrids_seds.gridsub0.hd5"
+    )
+    seds_sub1_fname_cache = download_rename(
+        "beast_example_phat_subgrids_seds.gridsub1.hd5"
+    )
     noise_fname_cache = download_rename("beast_example_phat_noisemodel.grid.hd5")
     noise_trim_fname_cache = download_rename(
         "beast_example_phat_noisemodel_trim.grid.hd5"
@@ -1069,6 +1082,104 @@ class TestRegressionSuite:
         # compare to new table
         compare_tables(expected_star_prob, Table(star_prob))
 
+    # ###################################################################
+    # tools.run tests
+
+    def test_create_physicsmodel_no_subgrid():
+        """
+        Test create_physicsmodel, assuming no subgrids
+        """
+
+        # get beast settings mockup
+        settings = beast_settings_mockup.beast_settings_mockup()
+
+        # run create_physicsmodel
+        create_physicsmodel.create_physicsmodel(
+            settings, nsubs=settings.n_subgrid, nprocs=1
+        )
+
+        # check that files match
+        # - isochrones
+        table_cache = Table.read(
+            self.iso_fname_cache, format="ascii.csv", comment="#", delimiter=",",
+        )
+        table_new = Table.read(
+            "./beast_no_subgrid/beast_no_subgrid_iso.csv",
+            format="ascii.csv",
+            comment="#",
+            delimiter=",",
+        )
+        compare_tables(table_cache, table_new)
+        # - spectra with priors
+        compare_hdf5(
+            self.priors_fname_cache, "./beast_no_subgrid/beast_no_subgrid_spec_w_priors.grid.hd5"
+        )
+        # - SEDs grid
+        compare_hdf5(self.seds_fname_cache, "./beast_no_subgrid/beast_no_subgrid_seds.grid.hd5")
+
+    def test_create_physicsmodel_with_subgrid():
+        """
+        Test create_physicsmodel, assuming two subgrids
+        """
+
+        # get beast settings mockup
+        settings = beast_settings_mockup.beast_settings_mockup()
+        # edit to have 2 subgrids
+        settings.n_subgrid = 2
+        settings.project = "beast_example_phat_subgrids"
+
+        # run create_physicsmodel
+        create_physicsmodel.create_physicsmodel(
+            settings, nsubs=settings.n_subgrid, nprocs=1
+        )
+
+        # check that files match
+
+        # - isochrones
+        table_cache = Table.read(
+            self.iso_fname_cache, format="ascii.csv", comment="#", delimiter=",",
+        )
+        table_new = Table.read(
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_iso.csv",
+            format="ascii.csv",
+            comment="#",
+            delimiter=",",
+        )
+        compare_tables(table_cache, table_new)
+
+        # - spectra with priors
+        compare_hdf5(
+            self.priors_fname_cache,
+            "./beast_example_phat_subgrids/beast_example_phat_subgrids_spec_w_priors.grid.hd5",
+        )
+        compare_hdf5(
+            self.priors_sub0_fname_cache,
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_spec_w_priors.gridsub0.hd5",
+        )
+        compare_hdf5(
+            self.priors_sub1_fname_cache,
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_spec_w_priors.gridsub1.hd5",
+        )
+
+        # - SEDs grid
+        compare_hdf5(
+            self.seds_sub0_fname_cache,
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_seds.gridsub0.hd5",
+        )
+        compare_hdf5(
+            self.seds_sub1_fname_cache,
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_seds.gridsub1.hd5",
+        )
+
+        # - list of subgrids
+        with open("./beast_example_phat_subgrids/subgrid_fnames.txt") as f:
+            temp = f.read()
+        subgrid_list = [x for x in temp.split("\n") if x != ""]
+        expected_list = [
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_seds.gridsub0.hd5",
+            "beast_example_phat_subgrids/beast_example_phat_subgrids_seds.gridsub1.hd5",
+        ]
+        assert subgrid_list == expected_list, "subgrid_fnames.txt has incorrect content"
 
 # specific helper functions
 def split_and_check(grid_fname, num_subgrids):
