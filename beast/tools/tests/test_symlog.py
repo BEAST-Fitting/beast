e1fa910bcc707d00c240584d8027863c8595bbab
Karl Gordon <kgordon@stsci.edu>
2020-09-15 13:16:58 -0400

    minor update

--- beast/tools/tests/test_symlog.py
+++ beast/tools/tests/test_symlog.py
@@ -19,6 +19,8 @@ def test_symlog():
         1.00000000e01,
     ]
 
+    # test going to symlog
     np.allclose(symlog(y), exp_symlog)
 
+    # test converting back to liner
     np.allclose(inverse_symlog(exp_symlog), y)
