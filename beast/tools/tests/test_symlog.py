a6d91d2b53d55aff7cc10dc81291f75cf1711596
Karl Gordon <kgordon@stsci.edu>
2020-09-15 12:22:05 -0400

    adding in symlog tests

---
+++ beast/tools/tests/test_symlog.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from beast.tools.symlog import symlog, inverse_symlog
+
+
+def test_symlog():
+
+    y = np.array([-1e-10, -1e-19, -1e-20, -1e-24, 0.0, 1e-24, 1e-20, 1e-19, 1e-10])
+
+    exp_symlog = [
+        -1.00000000e01,
+        -1.04139269e00,
+        -3.01029996e-01,
+        -4.34272769e-05,
+        0.00000000e00,
+        4.34272769e-05,
+        3.01029996e-01,
+        1.04139269e00,
+        1.00000000e01,
+    ]
+
+    np.allclose(symlog(y), exp_symlog)
+
+    np.allclose(inverse_symlog(exp_symlog), y)
