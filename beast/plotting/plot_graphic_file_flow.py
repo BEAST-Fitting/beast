0eb440451d685f8ad9fb4300b9f935eb2ef34930
Lea Hagen <lhagen@stsci.edu>
2020-12-15 17:48:59 -0500

    adjustments to force files to line up properly

--- beast/plotting/plot_graphic_file_flow.py
+++ beast/plotting/plot_graphic_file_flow.py
@@ -22,8 +22,25 @@ def plot_graphic_file_flow(n_sd=1, n_sub=3, savefig="png"):
         set to the file extension of desired file to save image of model
     """
 
-    # initialize dict of nodes
-    nodes = {}
+    # initialize graph
+    # (note: can't do dictionary for nodes like plot_graphic_model.py, because
+    # we need to exert more control over which things are lined up)
+    graph = Digraph(node_attr={'shape': 'box'})
+
+    # first layer
+    with graph.subgraph() as sg:
+        sg.attr(rank='same')
+        sg.node('sed','SEDgrid')
+        sg.node('phot','phot')
+        sg.node('fake','phot_fake')
+
+    # additional layers
+    sg1 = Digraph(node_attr={'shape': 'box'})
+    sg2 = Digraph(node_attr={'shape': 'box'})
+    sg3 = Digraph(node_attr={'shape': 'box'})
+    sg4 = Digraph(node_attr={'shape': 'box'})
+    sg5 = Digraph(node_attr={'shape': 'box'})
+    _ = [x.attr(rank='same') for x in [sg1, sg2, sg3, sg4, sg5]]
 
     # initialize dict of edges
     edges = defaultdict(list)
@@ -34,9 +51,9 @@ def plot_graphic_file_flow(n_sd=1, n_sub=3, savefig="png"):
         curr_sd = f'SD{s}-{s+1}'
 
         # files for this source density bin
-        nodes[f'phot{s}'] = f'phot_{curr_sd}'
-        nodes[f'fake{s}'] = f'phot_fake_{curr_sd}'
-        nodes[f'obs{s}'] = f'obsmodel_{curr_sd}'
+        sg1.node(f'phot{s}', f'phot_{curr_sd}')
+        sg1.node(f'fake{s}', f'phot_fake_{curr_sd}')
+        sg2.node(f'obs{s}', f'obsmodel_{curr_sd}')
 
         # make edges
         edges['phot'].append(f'phot{s}')
@@ -48,20 +65,19 @@ def plot_graphic_file_flow(n_sd=1, n_sub=3, savefig="png"):
             curr_sub = f'sub{b}'
 
             # -- nodes --
-            nodes[f'phot{s}s{b}'] = f'phot_{curr_sd}_{curr_sub}'
-            nodes[f'sed{s}s{b}'] = f'SEDgrid_{curr_sd}_{curr_sub}_trim'
-            nodes[f'obs{s}s{b}'] = f'obsmodel_{curr_sd}_{curr_sub}_trim'
-            nodes[f'lnps_{s}s{b}'] = f'likelihoods for\n{curr_sd}_{curr_sub}'
-            nodes[f'stat{s}s{b}'] = f'stats_{curr_sd}_{curr_sub}'
-            nodes[f'pdf1d{s}s{b}'] = f'pdf1d_{curr_sd}_{curr_sub}'
-            nodes[f'pdf2d{s}s{b}'] = f'pdf2d_{curr_sd}_{curr_sub}'
-            nodes[f'lnp{s}s{b}'] = f'lnp_{curr_sd}_{curr_sub}'
+            sg3.node(f'phot{s}s{b}', f'phot_{curr_sd}_{curr_sub}')
+            sg3.node(f'sed{s}s{b}', f'SEDgrid_{curr_sd}_{curr_sub}_trim')
+            sg3.node(f'obs{s}s{b}', f'obsmodel_{curr_sd}_{curr_sub}_trim')
+            sg4.node(f'lnps_{s}s{b}', f'likelihoods for\n{curr_sd}_{curr_sub}')
+            sg5.node(f'stat{s}s{b}', f'stats_{curr_sd}_{curr_sub}')
+            sg5.node(f'pdf1d{s}s{b}', f'pdf1d_{curr_sd}_{curr_sub}')
+            sg5.node(f'pdf2d{s}s{b}', f'pdf2d_{curr_sd}_{curr_sub}')
+            sg5.node(f'lnp{s}s{b}', f'lnp_{curr_sd}_{curr_sub}')
+
 
             # -- edges --
             # phot to sub-phot
             edges[f'phot{s}'].append(f'phot{s}s{b}')
-            # sub-phot to trimmed SED/obsmodel
-            #edges[f'phot{s}s{b}'] += [f'sed{s}s{b}', f'obs{s}s{b}']
             # SED to trimmed SED
             edges['sed'].append(f'sed{s}s{b}')
             # obsmodel to trimmed obsmodel
@@ -75,25 +91,18 @@ def plot_graphic_file_flow(n_sd=1, n_sub=3, savefig="png"):
                 f'stat{s}s{b}', f'pdf1d{s}s{b}', f'pdf2d{s}s{b}', f'lnp{s}s{b}'
             ]
 
-    #import pdb; pdb.set_trace()
-    # initialize graph
-    graph = Digraph()
-
-    # first layer
-    with graph.subgraph() as sg:
-        sg.attr(rank='same')
-        sg.node('sed','SEDgrid')
-        sg.node('phot','phot')
-        sg.node('fake','phot_fake')
-
-    # add the rest of the nodes
-    for ckey in nodes.keys():
-        graph.node(ckey, nodes[ckey])
 
     # add the rest of the edges
     for ckey in edges.keys():
         for cval in edges[ckey]:
             graph.edge(ckey, cval)
+
+    # append subgraphs
+    graph.subgraph(sg1)
+    graph.subgraph(sg2)
+    graph.subgraph(sg3)
+    graph.subgraph(sg4)
+    graph.subgraph(sg5)
 
     # make the graph flow horizontally
     graph.graph_attr['rankdir'] = 'LR'
