ad7a38af2e88928e075d4785713f84b14cac6b8d
Christina Lindberg <christina.lindberg@live.com>
2021-04-07 17:13:24 -0400

    remove sedgrid for fetching filters

--- beast/plotting/plot_toothpick_details.py
+++ beast/plotting/plot_toothpick_details.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
+from astropy.table import Table
+
 from beast.observationmodel.noisemodel import toothpick
-from beast.physicsmodel.grid import SEDGrid
 from beast.plotting.beastplotlib import set_params
 
 __all__ = ["plot_toothpick_details"]
 
 
-def plot_toothpick_details(asts_filename, seds_filename, savefig=False):
+def plot_toothpick_details(asts_filename, savefig=False):
     """
     Plot the details of the toothpick noisemodel creation for each filter.
     These plots show the individual AST results as points as
@@ -22,16 +23,14 @@ def plot_toothpick_details(asts_filename, seds_filename, savefig=False):
     asts_filename : str
         filename with the AST results
 
-    seds_filename : str
-        filename with the SED grid (used just for the filter information)
-
     savefig : str (default=False)
         to save the figure, set this to the file extension (e.g., 'png', 'pdf')
     """
-    sedgrid = SEDGrid(seds_filename, backend="cache")
+    # determine filters
+    filters = fetch_filters(asts_filename)
 
     # read in AST results
-    model = toothpick.MultiFilterASTs(asts_filename, sedgrid.filters)
+    model = toothpick.MultiFilterASTs(asts_filename, filters)
 
     # set the column mappings as the external file is BAND_VEGA or BAND_IN
     model.set_data_mappings(upcase=True, in_pair=("in", "in"), out_pair=("out", "rate"))
@@ -43,12 +42,12 @@ def plot_toothpick_details(asts_filename, seds_filename, savefig=False):
         ast_nonrecovered_ratio=ast_nonrecovered_ratio,
     )
 
-    nfilters = len(sedgrid.filters)
+    nfilters = len(filters)
     figsize_y = nfilters * 3
     fig, ax = plt.subplots(nrows=nfilters, ncols=2, figsize=(14, figsize_y), sharex=True)
     set_params()
 
-    for i, cfilter in enumerate(sedgrid.filters):
+    for i, cfilter in enumerate(filters):
         mag_in = model.data[model.filter_aliases[cfilter + "_in"]]
         flux_out = model.data[model.filter_aliases[cfilter + "_out"]]
 
@@ -110,7 +109,7 @@ def plot_toothpick_details(asts_filename, seds_filename, savefig=False):
     # add in the zero line
     # do after all the data has been plotted to get the full x range
     pxrange = ax[0, 0].get_xlim()
-    for i, cfilter in enumerate(sedgrid.filters):
+    for i, cfilter in enumerate(filters):
         ax[i, 0].plot(pxrange, [1.0, 1.0], "k--", alpha=0.5)
 
     # figname
@@ -123,3 +122,58 @@ def plot_toothpick_details(asts_filename, seds_filename, savefig=False):
         fig.savefig("{}.{}".format(basename, savefig))
     else:
         plt.show()
+
+def fetch_filters(filename):
+    """
+    For any FITS table file (gst, ast, etc.), collect the filters based on the
+    columns containing "VEGA".
+
+    Parameters
+    ----------
+    filename : str
+        filename containing VEGA measurements
+
+    Returns
+    -------
+    filters : list
+        list of full filter names
+
+    """
+
+    # all possible filters
+    gst_filter_names = [
+        "F225W",
+        "F275W",
+        "F336W",
+        "F475W",
+        "F814W",
+        "F110W",
+        "F160W",
+        "F657N",
+    ]
+    beast_filter_names = [
+        "HST_WFC3_F225W",
+        "HST_WFC3_F275W",
+        "HST_WFC3_F336W",
+        "HST_WFC3_F475W",
+        "HST_WFC3_F814W",
+        "HST_WFC3_F110W",
+        "HST_WFC3_F160W",
+        "HST_WFC3_F657N",
+    ]
+
+    data = Table.read(filename)
+
+    # extract every filter mentioned in the table
+    # find all columns mentioning VEGA
+    filter_cols = [c for c in data.colnames if "VEGA" in c]
+    filters = [f.split("_")[0] for f in filter_cols]
+
+    # match with the gst filter list
+    filter_ids = [gst_filter_names.index(i) for i in filters]
+    filter_ids.sort()
+
+    gst_filter_names = [gst_filter_names[i] for i in filter_ids]
+    beast_filter_names = [beast_filter_names[i] for i in filter_ids]
+
+    return beast_filter_names
