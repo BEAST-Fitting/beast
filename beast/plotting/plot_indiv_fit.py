de19f98960be4caaaa20abb4b325563bcfa0af8c
Karl Gordon <kgordon@stsci.edu>
2023-02-16 13:55:34 -0500

    tox version bump and fix for numpy deprecation

--- beast/plotting/plot_indiv_fit.py
+++ beast/plotting/plot_indiv_fit.py
@@ -218,10 +218,10 @@ def plot_indiv_fit(filebase, starnum=0, savefig=False, plotfig=True):
 
     # get the observations
     waves *= 1e-4
-    obs_flux = np.zeros((n_filters), dtype=np.float)
-    mod_flux = np.zeros((n_filters, 3), dtype=np.float)
-    mod_flux_nd = np.zeros((n_filters, 3), dtype=np.float)
-    mod_flux_wbias = np.zeros((n_filters, 3), dtype=np.float)
+    obs_flux = np.zeros((n_filters), dtype=float)
+    mod_flux = np.zeros((n_filters, 3), dtype=float)
+    mod_flux_nd = np.zeros((n_filters, 3), dtype=float)
+    mod_flux_wbias = np.zeros((n_filters, 3), dtype=float)
     k = starnum
 
     corname = stats["Name"][k]
