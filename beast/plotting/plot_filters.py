105bb6f328c192b3f5fd775e0f0140365e7c4c7a
Karl Gordon <kgordon@stsci.edu>
2024-02-14 15:37:38 -0500

    adding in ACS HRC filters and enhancing the filter plotting routine

--- beast/plotting/plot_filters.py
+++ beast/plotting/plot_filters.py
@@ -16,7 +16,7 @@ def plot_filters(
     filter_names,
     filterLib=None,
     save_name="beast_filters",
-    xlim=[1.4e3, 2e4],
+    xlim=[3e3, 1e3],
     ylim=[1e-4, 2],
     show_plot=True,
 ):
@@ -50,8 +50,11 @@ def plot_filters(
     )
 
     color_indices = np.log10(np.array(np.sort([f.norm for f in flist])))
-    color_indices -= color_indices.min()
-    color_indices /= color_indices.max()
+    if len(color_indices) > 1:
+        color_indices -= color_indices.min()
+        color_indices /= color_indices.max()
+    else:
+        color_indices = [0.0]
 
     cmap = mpl.cm.plasma
     # ax.set_prop_cycle(color=[cmap(i) for i in color_indices])
@@ -61,19 +64,25 @@ def plot_filters(
         c = next(color)
         ax.plot(f.wavelength, f.transmit, color=c, lw=2)
         ax.fill_between(f.wavelength, f.transmit, alpha=0.2, color=c)
+        yval_text = max(f.transmit * 0.1)
         ax.text(
-            np.nanmean(f.wavelength[f.transmit > 100.0 * ylim[0]]),
-            1.3 * np.nanmax(f.transmit[f.transmit > ylim[0]]),
+            np.nanmean(f.wavelength[f.transmit > yval_text]),
+            1.3 * np.nanmax(f.transmit[f.transmit > yval_text]),
             f.name.split("_")[-1],
             ha="center",
             color=c,
         )
+        gvals = (f.transmit > ylim[0]) & (f.transmit < ylim[1])
+        if min(f.wavelength[gvals]) < xlim[0]:
+            xlim[0] = min(f.wavelength[gvals])
+        if max(f.wavelength[gvals]) > xlim[1]:
+            xlim[1] = max(f.wavelength[gvals])
 
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
-    ax.set_xlabel(r"$\lambda$ [$\mu m$]")
+    ax.set_xlabel(r"$\lambda$ [$\AA$]")
     ax.set_ylabel(r"$B_i(\lambda)$")
 
     # ax.set_xticks([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0])
@@ -95,9 +104,7 @@ if __name__ == "__main__":  # pragma: no cover
         default="filters.appendVegaFilter",
         help="Save figure to file",
     )
-    args = parser.parse_args()
-
-    filter_names = [
+    def_filter_names = [
         "HST_WFC3_F225W",
         "HST_WFC3_F275W",
         "HST_WFC3_F336W",
@@ -107,8 +114,13 @@ if __name__ == "__main__":  # pragma: no cover
         "HST_WFC3_F110W",
         "HST_WFC3_F160W",
     ]
+    parser.add_argument("filter_names",
+                        help="names of filters",
+                        nargs='+',
+                        default=def_filter_names)
+    args = parser.parse_args()
 
-    fig = plot_filters(filter_names, show_plot=False)
+    fig = plot_filters(args.filter_names, show_plot=False)
 
     if args.tex:
         plt.rc({"usetex": True})
