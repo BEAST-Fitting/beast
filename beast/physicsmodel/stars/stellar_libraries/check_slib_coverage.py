7fd8448eda8ee2c0ec55cca809e7ffeceff658c5
Karl Gordon <kgordon@stsci.edu>
2026-05-25 12:14:51 -0400

    last codestlye

--- beast/physicsmodel/stars/stellar_libraries/check_slib_coverage.py
+++ beast/physicsmodel/stars/stellar_libraries/check_slib_coverage.py
@@ -13,15 +13,15 @@ if __name__ == "__main__":  # pragma: no cover
 
     for cz in np.unique(slib.Z.data):
         bound = get_stellib_boundaries(slib, dlogT=0.0, dlogg=0.0)
-        #bbox = slib.get_boundaries(dlogT=0.01, dlogg=0.01)
-        #print(bbox)
-        #print(bound)
+        # bbox = slib.get_boundaries(dlogT=0.01, dlogg=0.01)
+        # print(bbox)
+        # print(bound)
 
         gvals = slib.Z.data == cz
         plt.plot(slib.logT[gvals], slib.logg[gvals], "ko")
         plt.plot(bound[:, 0], bound[:, 1], "b-", label="new")
         slib.plot_boundary(dlogT=0.0, dlogg=0.0)
-        #plt.plot(bbox[:, 0], bbox[:, 1], "g-", label="in lib")
+        # plt.plot(bbox[:, 0], bbox[:, 1], "g-", label="in lib")
         plt.title(cz)
         plt.legend()
         plt.show()
